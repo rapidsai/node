54f0f3a2f49bf4333d3691085c3dd59082dbcf32
Christopher Harris <xixonia@gmail.com>
2020-09-14 19:09:48 -0500

    update based on @rapidsai/cuda module updates.

--- modules/ucx/index.js
+++ modules/ucx/index.js
@@ -1,6 +1,1 @@
-// process.env.LD_LIBRARY_PATH = [
-//     process.env.LD_LIBRARY_PATH,
-//     require('path').join(__dirname, 'build', 'lib')
-// ].filter(Boolean).join(':')
-
 module.exports = require('./build/js/index');
