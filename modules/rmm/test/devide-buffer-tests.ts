eb4592ce163aaeb7da33db9dc72b9abce4ea3592
Bryan Van de Ven <bryan@bokeh.org>
2020-09-28 15:14:40 -0700

    Update modules/rmm/test/devide-buffer-tests.ts
    Co-authored-by: Paul Taylor <paul.e.taylor@me.com>

--- modules/rmm/test/devide-buffer-tests.ts
+++ modules/rmm/test/devide-buffer-tests.ts
@@ -3,16 +3,16 @@ import { DeviceBuffer } from '@nvidia/rmm'
 import { devices, CUDADevice, CUDADeviceFlag } from '@nvidia/cuda';
 
 test(`DeviceBuffer initialization`, () => {
-    const db = new DeviceBuffer()
-    expect(db.byteLength).toBe(0)
-    expect(db.capacity).toBe(0)
-    expect(db.isEmpty).toBe(true)
-})
+    const db = new DeviceBuffer();
+    expect(db.byteLength).toBe(0);
+    expect(db.capacity).toBe(0);
+    expect(db.isEmpty).toBe(true);
+});
 
 test(`DeviceBuffer resize`, () => {
-    const db = new DeviceBuffer()
-    db.resize(1234)
-    expect(db.byteLength).toBe(1234)
-    expect(db.capacity).toBe(1234)
-    expect(db.isEmpty).toBe(false)
-})
+    const db = new DeviceBuffer();
+    db.resize(1234);
+    expect(db.byteLength).toBe(1234);
+    expect(db.capacity).toBe(1234);
+    expect(db.isEmpty).toBe(false);
+});
