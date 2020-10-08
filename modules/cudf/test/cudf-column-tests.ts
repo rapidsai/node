7ef36596309b981573b648254795146edd30b720
Ajay Thorve <athorve@nvidia.com>
2020-10-08 13:37:03 -0700

    added tests

--- modules/cudf/test/cudf-column-tests.ts
+++ modules/cudf/test/cudf-column-tests.ts
@@ -9,8 +9,8 @@ test('Column initialization', () => {
 
     expect(col.type()).toBe(types.INT32);
     expect(col.size()).toBe(buffer_size);
-    expect(col.null_count()).toBe(0);
-    expect(col.has_nulls()).toBe(false);
+    expect(col.nullCount()).toBe(0);
+    expect(col.hasNulls()).toBe(false);
     expect(col.nullable()).toBe(false);
 });
 
@@ -23,15 +23,13 @@ test('Column initialization with null_mask', () => {
 
     expect(col.type()).toBe(types.BOOL8);
     expect(col.size()).toBe(buffer_size);
-    expect(col.null_count()).toBe(100);
-    expect(col.has_nulls()).toBe(true);
+    expect(col.nullCount()).toBe(100);
+    expect(col.hasNulls()).toBe(true);
     expect(col.nullable()).toBe(true);
 });
 
 
-
-
-test('Column initialization with null_mask, null_count', () => {
+test('Column null_mask, null_count', () => {
     const buffer_size = 100;
     const db = new DeviceBuffer(buffer_size,10);
     const null_mask = new DeviceBuffer(buffer_size,10);
@@ -39,7 +37,39 @@ test('Column initialization with null_mask, null_count', () => {
 
     expect(col.type()).toBe(types.FLOAT32);
     expect(col.size()).toBe(buffer_size);
-    expect(col.null_count()).toBe(1);
-    expect(col.has_nulls()).toBe(true);
+    expect(col.nullCount()).toBe(1);
+    expect(col.hasNulls()).toBe(true);
     expect(col.nullable()).toBe(true);
+});
+
+
+test('test child(child_index), num_children', () => {
+    const buffer_size = 100;
+    const db = new DeviceBuffer(buffer_size,10);
+    const db1 = new DeviceBuffer(buffer_size*2,10);
+    const null_mask = new DeviceBuffer(buffer_size,10);
+    const col = new Column(types.FLOAT32, 10, db, null_mask, 1);
+
+    const col1 = new Column(types.FLOAT64, 10, db1, null_mask, 20, [col]);
+
+    expect(col1.type()).toBe(types.FLOAT64);
+    expect(col1.numChildren()).toBe(1);
+    expect(col1.child(0).size()).toBe(col.size());
+    expect(col1.child(0).type()).toBe(col.type());
+});
+
+
+test('test Column(column) constructor', () => {
+    const buffer_size = 100;
+    const db = new DeviceBuffer(buffer_size,10);
+
+    const null_mask = new DeviceBuffer(buffer_size,10);
+    const col = new Column(types.FLOAT32, 10, db, null_mask, 1);
+    const col1 = new Column(col);
+
+    expect(col1.type()).toBe(types.FLOAT32);
+    expect(col1.size()).toBe(buffer_size);
+    expect(col1.nullCount()).toBe(1);
+    expect(col1.hasNulls()).toBe(true);
+    expect(col1.nullable()).toBe(true);
 });
