9c590af2c5351d502a3c7abda93a1164e8d67ed9
Bryan Van de Ven <bryan@bokeh.org>
2020-10-12 20:48:31 +0000

    update tests

--- modules/rmm/test/device-buffer-tests.ts
+++ modules/rmm/test/device-buffer-tests.ts
@@ -1,16 +1,28 @@
 import { DeviceBuffer } from '@nvidia/rmm';
 
-test(`DeviceBuffer initialization`, () => {
-    const db = new DeviceBuffer();
+test(`DeviceBuffer empty initialization`, () => {
+    const db = new DeviceBuffer(0);
     expect(db.byteLength).toBe(0);
     expect(db.capacity).toBe(0);
     expect(db.isEmpty).toBe(true);
 });
 
+test(`DeviceBuffer initialization`, () => {
+    const db = new DeviceBuffer(1000);
+    expect(db.byteLength).toBe(1000);
+    expect(db.capacity).toBe(1000);
+    expect(db.isEmpty).toBe(false);
+});
+
 test(`DeviceBuffer resize`, () => {
-    const db = new DeviceBuffer();
+    const db = new DeviceBuffer(1000);
     db.resize(1234);
     expect(db.byteLength).toBe(1234);
     expect(db.capacity).toBe(1234);
     expect(db.isEmpty).toBe(false);
+
+    db.resize(0);
+    expect(db.byteLength).toBe(0);
+    expect(db.capacity).toBe(1234);
+    expect(db.isEmpty).toBe(true);
 });
