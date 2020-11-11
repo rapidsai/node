5fc2293f311529e3e2a6f1a33e2f283555e3ca84
Ajay Thorve <athorve@nvidia.com>
2020-11-11 12:09:15 -0800

    updated tests

--- modules/cudf/test/cudf-table-tests.ts
+++ modules/cudf/test/cudf-table-tests.ts
@@ -15,7 +15,7 @@ test('Table initialization', () => {
         data: new Uint8Buffer(length),
         nullMask: new Uint8Buffer(64),
     });
-    const table_0 = new Table({"col_0":col_0, "col_1": col_1});
+    const table_0 = new Table({data:{"col_0":col_0, "col_1": col_1}});
     expect(table_0.numColumns).toBe(2);
     expect(table_0.numRows).toBe(length);
     expect(table_0.columns).toStrictEqual([ 'col_0', 'col_1' ]);
@@ -37,15 +37,16 @@ test('Table.select', () => {
     const col_2 = new Column({ type: TypeId.INT32, data: new Int32Buffer(length) });
     const col_3 = new Column({ type: TypeId.INT32, data: new Int32Buffer(length) });
 
-    const table_0 = new Table({"col_0":col_0, "col_1": col_1, "col_2": col_2, "col_3": col_3});
+    const table_0 = new Table(
+        {data:{"col_0":col_0, "col_1": col_1, "col_2": col_2, "col_3": col_3}});
     
     expect(table_0.numColumns).toBe(4);
     expect(table_0.numRows).toBe(length);
     expect(table_0.columns).toStrictEqual([ "col_0", "col_1", "col_2", "col_3"]);
     
-    expect(table_0.select(["col_0"])).toStrictEqual(new Table({"col_0": col_0}));
+    expect(table_0.select(["col_0"])).toStrictEqual(new Table({data:{"col_0": col_0}}));
     expect(table_0.select(["col_0", "col_3"])).toStrictEqual(
-        new Table({"col_0": col_0, "col_3": col_3})
+        new Table({data:{"col_0": col_0, "col_3": col_3}})
     );
 });
 
@@ -63,17 +64,17 @@ test('Table.slice', () => {
     const col_2 = new Column({ type: TypeId.INT32, data: new Int32Buffer(length) });
     const col_3 = new Column({ type: TypeId.INT32, data: new Int32Buffer(length) });
 
-    const table_0 = new Table({"col_0":col_0, "col_1": col_1, "col_2": col_2, "col_3": col_3});
+    const table_0 = new Table({data:{"col_0":col_0, "col_1": col_1, "col_2": col_2, "col_3": col_3}});
     
     expect(table_0.numColumns).toBe(4);
     expect(table_0.numRows).toBe(length);
     expect(table_0.columns).toStrictEqual([ "col_0", "col_1", "col_2", "col_3"]);
     
     expect(table_0.slice(2, 3)).toStrictEqual(
-        new Table({"col_2": col_0, "col_3": col_3})
+        new Table({data:{"col_2": col_0, "col_3": col_3}})
     );
     expect(table_0.slice("col_1", "col_3")).toStrictEqual(
-        new Table({"col_1": col_1, "col_2": col_2, "col_3": col_3})
+        new Table({data:{"col_1": col_1, "col_2": col_2, "col_3": col_3}})
     );
 
 });
