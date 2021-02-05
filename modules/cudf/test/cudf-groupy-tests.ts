b09f8651747096880521cf22f9e40de47e271c16
Bryan Van de Ven <bryan@bokeh.org>
2021-02-05 15:27:44 -0800

    add remaining libcudf tests

--- modules/cudf/test/cudf-groupy-tests.ts
+++ modules/cudf/test/cudf-groupy-tests.ts
@@ -12,9 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {setDefaultAllocator} from '@nvidia/cuda';
+import {setDefaultAllocator, Uint8Buffer} from '@nvidia/cuda';
 import {DataFrame, GroupBy, Int32, Series} from '@nvidia/cudf';
 import {CudaMemoryResource, DeviceBuffer} from '@nvidia/rmm';
+import {BoolVector} from 'apache-arrow'
 
 const mr = new CudaMemoryResource();
 
@@ -26,12 +27,30 @@ test('Groupby basic', () => {
 
   const grp = new GroupBy({keys: df});
 
-  const groups = grp.getGroups()
+  const groups = grp.getGroups();
 
-  const keys_result = Series.new(groups["keys"].getColumnByIndex(0))
-  expect([...keys_result.toArrow()]).toEqual([1, 1, 1, 2, 2, 3])
+  const keys_result = Series.new(groups["keys"].getColumnByIndex(0));
+  expect([...keys_result.toArrow()]).toEqual([1, 1, 1, 2, 2, 3]);
 
-  expect([...groups["offsets"]]).toEqual([0, 3, 5, 6])
+  expect(groups["values"]).toBeUndefined();
+
+  expect([...groups["offsets"]]).toEqual([0, 3, 5, 6]);
+});
+
+test('Groupby empty', () => {
+  const a  = Series.new({type: new Int32(), data: new Int32Array([])});
+  const df = new DataFrame({"a": a});
+
+  const grp = new GroupBy({keys: df});
+
+  const groups = grp.getGroups();
+
+  const keys_result = Series.new(groups["keys"].getColumnByIndex(0));
+  expect(keys_result.length).toBe(0);
+
+  expect(groups["values"]).toBeUndefined();
+
+  expect([...groups["offsets"]]).toEqual([0]);
 });
 
 test('Groupby basic with values', () => {
@@ -43,13 +62,58 @@ test('Groupby basic with values', () => {
 
   const grp = new GroupBy({keys: df});
 
-  const groups = grp.getGroups(df2)
+  const groups = grp.getGroups(df2);
 
-  const keys_result = Series.new(groups["keys"].getColumnByIndex(0))
-  expect([...keys_result.toArrow()]).toEqual([0, 1, 2, 3, 4, 5])
+  const keys_result = Series.new(groups["keys"].getColumnByIndex(0));
+  expect([...keys_result.toArrow()]).toEqual([0, 1, 2, 3, 4, 5]);
 
-  const values_result = Series.new(groups["values"].getColumnByIndex(0))
-  expect([...values_result.toArrow()]).toEqual([2, 2, 1, 1, 0, 0])
+  const values_result = Series.new(groups["values"].getColumnByIndex(0));
+  expect([...values_result.toArrow()]).toEqual([2, 2, 1, 1, 0, 0]);
 
-  expect([...groups["offsets"]]).toEqual([0, 1, 2, 3, 4, 5, 6])
+  expect([...groups["offsets"]]).toEqual([0, 1, 2, 3, 4, 5, 6]);
+});
+
+test('Groupby all nulls', () => {
+  const a  = Series.new({
+    type: new Int32(),
+    data: new Int32Array([1, 1, 2, 3, 1, 2]),
+    nullMask: new Uint8Buffer(BoolVector.from([0, 0, 0, 0, 0, 0]).values),
+  });
+  const df = new DataFrame({"a": a});
+
+  const grp = new GroupBy({keys: df});
+
+  const groups = grp.getGroups();
+
+  const keys_result = Series.new(groups["keys"].getColumnByIndex(0));
+  expect(keys_result.length).toBe(0);
+
+  expect(groups["values"]).toBeUndefined();
+
+  expect([...groups["offsets"]]).toEqual([0]);
+});
+
+test('Groupby some nulls', () => {
+  const a  = Series.new({
+    type: new Int32(),
+    data: new Int32Array([1, 1, 3, 2, 1, 2]),
+    nullMask: new Uint8Buffer(BoolVector.from([1, 0, 1, 0, 0, 1]).values),
+  });
+  const df = new DataFrame({"a": a});
+
+  const b   = Series.new({type: new Int32(), data: new Int32Array([1, 2, 3, 4, 5, 6])});
+  const df2 = new DataFrame({"b": b});
+
+  const grp = new GroupBy({keys: df});
+
+  const groups = grp.getGroups(df2);
+
+  const keys_result = Series.new(groups["keys"].getColumnByIndex(0));
+  expect([...keys_result.toArrow()]).toEqual([1, 2, 3]);
+  expect(keys_result.nullCount).toBe(0)
+
+  const values_result = Series.new(groups["values"].getColumnByIndex(0));
+  expect([...values_result.toArrow()]).toEqual([1, 6, 3]);
+
+  expect([...groups["offsets"]]).toEqual([0, 1, 2, 3]);
 });
