6720ed67833a2a2ee1923ddb5f451888675b8d40
Bryan Van de Ven <bryan@bokeh.org>
2021-02-23 12:32:29 -0800

    add nth tests, use simpler series construction

--- modules/cudf/test/cudf-groupy-tests.ts
+++ modules/cudf/test/cudf-groupy-tests.ts
@@ -15,18 +15,17 @@
 
 import './jest-extensions'
 
-import {setDefaultAllocator, Uint8Buffer} from '@nvidia/cuda';
+import {setDefaultAllocator} from '@nvidia/cuda';
 import {DataFrame, DataType, Float64, GroupBy, Int32, Series} from '@nvidia/cudf';
 import {CudaMemoryResource, DeviceBuffer} from '@nvidia/rmm';
-import {BoolVector} from 'apache-arrow'
 
 const mr = new CudaMemoryResource();
 
 setDefaultAllocator((byteLength: number) => new DeviceBuffer(byteLength, mr));
 
 function makeBasicData(values: number[]): DataFrame {
-  const a = Series.new({type: new Int32(), data: new Int32Array([1, 2, 3, 1, 2, 2, 1, 3, 3, 2])});
-  const b = Series.new({type: new Float64(), data: new Float64Array(values)});
+  const a = Series.new({type: new Int32, data: [1, 2, 3, 1, 2, 2, 1, 3, 3, 2]});
+  const b = Series.new({type: new Float64, data: values});
   return new DataFrame({'a': a, 'b': b});
 }
 
@@ -35,7 +34,7 @@ function basicAggCompare<T extends {a: DataType, b: DataType}>(result: DataFrame
   const ra = result.get('a');
   const rb = result.get('b');
 
-  const a_expected = Series.new({type: new Int32(), data: new Int32Array([1, 2, 3])});
+  const a_expected = Series.new({type: new Int32, data: [1, 2, 3]});
   expect([...ra.toArrow()]).toEqual([...a_expected.toArrow()]);
 
   const b_expected = Series.new({type: rb.type, data: expected});
@@ -43,7 +42,7 @@ function basicAggCompare<T extends {a: DataType, b: DataType}>(result: DataFrame
 }
 
 test('Groupby basic', () => {
-  const a  = Series.new({type: new Int32(), data: new Int32Array([1, 1, 2, 1, 2, 3])});
+  const a  = Series.new({type: new Int32, data: [1, 1, 2, 1, 2, 3]});
   const df = new DataFrame({'a': a});
 
   const grp = new GroupBy({obj: df, by: ['a']});
@@ -59,8 +58,8 @@ test('Groupby basic', () => {
 });
 
 test('Groupby basic two columns', () => {
-  const a  = Series.new({type: new Int32(), data: new Int32Array([1, 1, 2, 1, 2, 3])});
-  const aa = Series.new({type: new Int32(), data: new Int32Array([4, 5, 4, 4, 4, 3])});
+  const a  = Series.new({type: new Int32, data: [1, 1, 2, 1, 2, 3]});
+  const aa = Series.new({type: new Int32, data: [4, 5, 4, 4, 4, 3]});
   const df = new DataFrame({'a': a, 'aa': aa});
 
   const grp = new GroupBy({obj: df, by: ['a', 'aa']});
@@ -79,7 +78,7 @@ test('Groupby basic two columns', () => {
 });
 
 test('Groupby empty', () => {
-  const a  = Series.new({type: new Int32(), data: new Int32Array([])});
+  const a  = Series.new({type: new Int32, data: []});
   const df = new DataFrame({'a': a});
 
   const grp = new GroupBy({obj: df, by: ['a']});
@@ -95,8 +94,8 @@ test('Groupby empty', () => {
 });
 
 test('Groupby basic with values', () => {
-  const a  = Series.new({type: new Int32(), data: new Int32Array([5, 4, 3, 2, 1, 0])});
-  const b  = Series.new({type: new Int32(), data: new Int32Array([0, 0, 1, 1, 2, 2])});
+  const a  = Series.new({type: new Int32, data: [5, 4, 3, 2, 1, 0]});
+  const b  = Series.new({type: new Int32, data: [0, 0, 1, 1, 2, 2]});
   const df = new DataFrame({'a': a, 'b': b});
 
   const grp = new GroupBy({obj: df, by: ['a']});
@@ -115,9 +114,9 @@ test('Groupby basic with values', () => {
 });
 
 test('Groupby basic two columns with values', () => {
-  const a  = Series.new({type: new Int32(), data: new Int32Array([5, 4, 3, 2, 1, 0])});
-  const aa = Series.new({type: new Int32(), data: new Int32Array([4, 5, 4, 4, 4, 3])});
-  const b  = Series.new({type: new Int32(), data: new Int32Array([0, 0, 1, 1, 2, 2])});
+  const a  = Series.new({type: new Int32, data: [5, 4, 3, 2, 1, 0]});
+  const aa = Series.new({type: new Int32, data: [4, 5, 4, 4, 4, 3]});
+  const b  = Series.new({type: new Int32, data: [0, 0, 1, 1, 2, 2]});
   const df = new DataFrame({'a': a, 'aa': aa, 'b': b});
 
   const grp = new GroupBy({obj: df, by: ['a', 'aa']});
@@ -140,9 +139,9 @@ test('Groupby basic two columns with values', () => {
 
 test('Groupby all nulls', () => {
   const a  = Series.new({
-    type: new Int32(),
-    data: new Int32Array([1, 1, 2, 3, 1, 2]),
-    nullMask: new Uint8Buffer(BoolVector.from([0, 0, 0, 0, 0, 0]).values),
+    type: new Int32,
+    data: [1, 1, 2, 3, 1, 2],
+    nullMask: [false, false, false, false, false, false],
   });
   const df = new DataFrame({'a': a});
 
@@ -160,11 +159,11 @@ test('Groupby all nulls', () => {
 
 test('Groupby some nulls', () => {
   const a  = Series.new({
-    type: new Int32(),
-    data: new Int32Array([1, 1, 3, 2, 1, 2]),
-    nullMask: new Uint8Buffer(BoolVector.from([1, 0, 1, 0, 0, 1]).values),
+    type: new Int32,
+    data: [1, 1, 3, 2, 1, 2],
+    nullMask: [true, false, true, false, false, true],
   });
-  const b  = Series.new({type: new Int32(), data: new Int32Array([1, 2, 3, 4, 5, 6])});
+  const b  = Series.new({type: new Int32, data: [1, 2, 3, 4, 5, 6]});
   const df = new DataFrame({'a': a, 'b': b});
 
   const grp = new GroupBy({obj: df, by: ['a']});
@@ -225,6 +224,26 @@ test('Groupby min basic', () => {
   basicAggCompare(grp.min(), [0, 1, 2]);
 });
 
+test('Groupby nth basic', () => {
+  const a   = Series.new({type: new Int32, data: [1, 1, 1, 2, 2, 2, 3, 3, 3]});
+  const b   = Series.new({type: new Float64, data: [1, 2, 3, 10, 20, 30, 100, 200, 300]});
+  const df  = new DataFrame({'a': a, 'b': b});
+  const grp = new GroupBy({obj: df, by: ['a']});
+  basicAggCompare(grp.nth(0), [1, 10, 100]);
+  basicAggCompare(grp.nth(1), [2, 20, 200]);
+  basicAggCompare(grp.nth(2), [3, 30, 300]);
+});
+
+test('Groupby nth uneven', () => {
+  const a      = Series.new({type: new Int32, data: [1, 1, 1, 2, 2, 2, 3, 3]});
+  const b      = Series.new({type: new Float64, data: [1, 2, 3, 10, 20, 30, 100, 200]});
+  const df     = new DataFrame({'a': a, 'b': b});
+  const grp    = new GroupBy({obj: df, by: ['a']});
+  const result = grp.nth(2)
+  basicAggCompare(result, [3, 30, 0]);
+  expect(result.get('b').nullCount).toBe(1)
+});
+
 test('Groupby nunique basic', () => {
   const df  = makeBasicData([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
   const grp = new GroupBy({obj: df, by: ['a']});
@@ -257,8 +276,8 @@ const BASIC_AGGS: BasicAggType[] =
 
 for (const agg of BASIC_AGGS) {
   test(`Groupby ${agg} empty`, () => {
-    const a      = Series.new({type: new Int32(), data: new Int32Array([])});
-    const b      = Series.new({type: new Float64(), data: new Float64Array([])});
+    const a      = Series.new({type: new Int32, data: []});
+    const b      = Series.new({type: new Float64, data: []});
     const df     = new DataFrame({'a': a, 'b': b});
     const grp    = new GroupBy({obj: df, by: ['a']});
     const result = grp[agg]();
@@ -266,12 +285,21 @@ for (const agg of BASIC_AGGS) {
     expect(result.get('b').length).toBe(0);
   });
 }
+
+test(`Groupby nth empty`, () => {
+  const a      = Series.new({type: new Int32, data: []});
+  const b      = Series.new({type: new Float64, data: []});
+  const df     = new DataFrame({'a': a, 'b': b});
+  const grp    = new GroupBy({obj: df, by: ['a']});
+  const result = grp.nth(0);
+  expect(result.get('a').length).toBe(0);
+  expect(result.get('b').length).toBe(0);
+});
 
 for (const agg of BASIC_AGGS) {
   test(`Groupby ${agg} null keys`, () => {
-    const mask   = new Uint8Buffer(BoolVector.from([0, 0, 0]).values);
-    const a      = Series.new({type: new Int32(), data: new Int32Array([1, 2, 3]), nullMask: mask});
-    const b      = Series.new({type: new Float64(), data: new Float64Array([3, 4, 5])});
+    const a      = Series.new({type: new Int32, data: [1, 2, 3], nullMask: [false, false, false]});
+    const b      = Series.new({type: new Float64, data: [3, 4, 5]});
     const df     = new DataFrame({'a': a, 'b': b});
     const grp    = new GroupBy({obj: df, by: ['a']});
     const result = grp[agg]();
@@ -280,13 +308,22 @@ for (const agg of BASIC_AGGS) {
   });
 }
 
+test(`Groupby nth null keys`, () => {
+  const a      = Series.new({type: new Int32, data: [1, 2, 3], nullMask: [false, false, false]});
+  const b      = Series.new({type: new Float64, data: [3, 4, 5]});
+  const df     = new DataFrame({'a': a, 'b': b});
+  const grp    = new GroupBy({obj: df, by: ['a']});
+  const result = grp.nth(0);
+  expect(result.get('a').length).toBe(0);
+  expect(result.get('b').length).toBe(0);
+});
+
 for (const agg of BASIC_AGGS) {
   test(`Groupby ${agg} null values`, () => {
-    const mask = new Uint8Buffer(BoolVector.from([0, 0, 0]).values);
-    const a    = Series.new({type: new Int32(), data: new Int32Array([1, 1, 1])});
-    const b  = Series.new({type: new Float64(), data: new Float64Array([3, 4, 5]), nullMask: mask});
-    const df = new DataFrame({'a': a, 'b': b});
-    const grp    = new GroupBy({obj: df, by: ['a']});
+    const a   = Series.new({type: new Int32, data: [1, 1, 1]});
+    const b   = Series.new({type: new Float64, data: [3, 4, 5], nullMask: [false, false, false]});
+    const df  = new DataFrame({'a': a, 'b': b});
+    const grp = new GroupBy({obj: df, by: ['a']});
     const result = grp[agg]();
     expect([...result.get('a').toArrow()]).toEqual([1]);
     expect(result.get('a').nullCount).toBe(0);
@@ -298,3 +335,15 @@ for (const agg of BASIC_AGGS) {
     }
   });
 }
+
+test(`Groupby nth null values`, () => {
+  const a      = Series.new({type: new Int32, data: [1, 1, 1]});
+  const b      = Series.new({type: new Float64, data: [3, 4, 5], nullMask: [false, false, false]});
+  const df     = new DataFrame({'a': a, 'b': b});
+  const grp    = new GroupBy({obj: df, by: ['a']});
+  const result = grp.nth(0);
+  expect([...result.get('a').toArrow()]).toEqual([1]);
+  expect(result.get('a').nullCount).toBe(0);
+  expect(result.get('b').length).toBe(1);
+  expect(result.get('b').nullCount).toBe(1);
+});
