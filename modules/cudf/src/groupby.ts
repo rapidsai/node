8de55c68503489087ba37dd2b07e678ccea60853
Bryan Van de Ven <bryan@bokeh.org>
2021-03-10 13:37:23 -0800

    naive GroupBySingle / GroupByMultiple split

--- modules/cudf/src/groupby.ts
+++ modules/cudf/src/groupby.ts
@@ -48,22 +48,26 @@ type Join<T extends unknown[], D extends string> =
                                                 ? `${T[0]}${D}${Join<U, D>}`
                                                 : string;
 
-export type GroupByProps<T extends TypeMap, R extends keyof T> = {
-  obj: DataFrame<T>,
-  by: R[],
+type GroupByBaseProps = {
   include_nulls?: boolean,
   keys_are_sorted?: boolean,
   column_order?: boolean[],
   null_precedence?: NullOrder[],
-};
+}
+
+export type GroupBySingleProps<T extends TypeMap, R extends keyof T> = {
+  obj: DataFrame<T>,
+  by: R,
+}&GroupByBaseProps;
+
+export type GroupByMultipleProps<T extends TypeMap, R extends keyof T> = {
+  obj: DataFrame<T>,
+  by: R[],
+}&GroupByBaseProps;
 
 type CudfGroupByProps = {
   keys: Table,
-  include_nulls?: boolean,
-  keys_are_sorted?: boolean,
-  column_order?: boolean[],
-  null_precedence?: NullOrder[],
-};
+}&GroupByBaseProps;
 
 export type Groups<KeysMap extends TypeMap, ValuesMap extends TypeMap> = {
   keys: DataFrame<KeysMap>,
@@ -96,12 +100,213 @@ interface CudfGroupBy {
     {keys: Table, cols: [Column]};
 }
 
-export class GroupBy<T extends TypeMap, R extends keyof T> extends(
+export class GroupBySingle<T extends TypeMap, R extends keyof T> extends(
   <GroupbyConstructor>CUDF.GroupBy) {
   private _by: R[];
   private _values: DataFrame<Omit<T, R>>;
 
-  constructor(props: GroupByProps<T, R>) {
+  constructor(props: GroupBySingleProps<T, R>) {
+    const table = props.obj.select([props.by]).asTable();
+    const {
+      include_nulls   = false,
+      keys_are_sorted = false,
+      column_order    = [],
+      null_precedence = []
+    } = props;
+
+    const cudf_props: CudfGroupByProps = {
+      keys: table,
+      include_nulls: include_nulls,
+      keys_are_sorted: keys_are_sorted,
+      column_order: column_order,
+      null_precedence: null_precedence,
+    };
+    super(cudf_props);
+    this._by     = [props.by];
+    this._values = props.obj.drop([props.by]);
+  }
+
+  /**
+   * Return the Groups for this GroupBy
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  getGroups(memoryResource?: MemoryResource) {
+    const {keys, offsets, values} = this._getGroups(this._values.asTable(), memoryResource);
+
+    const results = {
+      offsets,
+      keys: new DataFrame(
+        this._by.reduce((keys_map, name, index) =>
+                          ({...keys_map, [name]: Series.new(keys.getColumnByIndex(index))}),
+                        {} as SeriesMap<Pick<T, R>>))
+    } as Groups<Pick<T, R>, Omit<T, R>>;
+
+    if (values !== undefined) {
+      results.values = new DataFrame(this._values.names.reduce(
+        (values_map, name, index) =>
+          ({...values_map, [name]: Series.new(values.getColumnByIndex(index))}),
+        {} as SeriesMap<Omit<T, R>>));
+    }
+
+    return results;
+  }
+
+  protected prepare_results(results: {keys: Table, cols: Column[]}) {
+    const {keys, cols} = results;
+    const series_map   = {} as SeriesMap<T>;
+
+    this._values.names.forEach((name, index) => { series_map[name] = Series.new(cols[index]); });
+    series_map[this._by[0]] = Series.new(keys.getColumnByIndex(0));
+
+    return new DataFrame(series_map);
+  }
+
+  /**
+   * Compute the index of the maximum value in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  argmax(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._argmax(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the index of the minimum value in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  argmin(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._argmin(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the size of each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  count(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._count(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the maximum value in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  max(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._max(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the average value each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  mean(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._mean(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the median value in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  median(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._median(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the minimum value in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  min(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._min(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Return the nth value from each group
+   *
+   * @param n the index of the element to return
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  nth(n: number, memoryResource?: MemoryResource) {
+    return this.prepare_results(this._nth(n, this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the number of unique values in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  nunique(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._nunique(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the standard deviation for each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  std(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._std(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the sum of values in each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  sum(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._sum(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Compute the variance for each group
+   *
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  var(memoryResource?: MemoryResource) {
+    return this.prepare_results(this._var(this._values.asTable(), memoryResource));
+  }
+
+  /**
+   * Return values at the given quantile.
+   *
+   * @param q the quantile to compute, 0 <= q <= 1
+   * @param interpolation This optional parameter specifies the interpolation method to use,
+   *  when the desired quantile lies between two data points i and j.
+   * @param memoryResource The optional MemoryResource used to allocate the result's
+   *   device memory.
+   */
+  quantile(q                                         = 0.5,
+           interpolation: keyof typeof Interpolation = 'linear',
+           memoryResource?: MemoryResource) {
+    return this.prepare_results(
+      this._quantile(q, this._values.asTable(), Interpolation[interpolation], memoryResource));
+  }
+}
+
+export class GroupByMultiple<T extends TypeMap, R extends keyof T> extends(
+  <GroupbyConstructor>CUDF.GroupBy) {
+  private _by: R[];
+  private _values: DataFrame<Omit<T, R>>;
+
+  constructor(props: GroupByMultipleProps<T, R>) {
     const table = props.obj.select(props.by).asTable();
     const {
       include_nulls   = false,
@@ -150,24 +355,6 @@ export class GroupBy<T extends TypeMap, R extends keyof T> extends(
   }
 
   protected prepare_results(results: {keys: Table, cols: Column[]}) {
-    if (this._by.length == 1) {
-      return this.prepare_results_single(results);
-    } else {
-      return this.prepare_results_multiple(results);
-    }
-  }
-
-  protected prepare_results_single(results: {keys: Table, cols: Column[]}) {
-    const {keys, cols} = results;
-    const series_map   = {} as SeriesMap<T>;
-
-    this._values.names.forEach((name, index) => { series_map[name] = Series.new(cols[index]); });
-    series_map[this._by[0]] = Series.new(keys.getColumnByIndex(0));
-
-    return new DataFrame(series_map);
-  }
-
-  protected prepare_results_multiple(results: {keys: Table, cols: Column[]}) {
     const {keys, cols} = results;
 
     type Subset   = Pick<T, R>;
@@ -182,22 +369,20 @@ export class GroupBy<T extends TypeMap, R extends keyof T> extends(
 
     const byname: keyof IndexMap = this._by.join('_');
 
-    if (byname in rest_map) {
-      throw new Error(`Groupby column name ${byname} already
-      exists`);
-    }
+    if (byname in rest_map) { throw new Error(`Groupby column name ${byname} already exists`); }
 
     const fields   = [];
     const children = [];
     for (const [index, name] of this._by.entries()) {
       const child = keys.getColumnByIndex(index)
       fields.push(Field.new({name: name as string, type: child.type}));
-      children.push(Series.new(child));
+      const series = Series.new(child);
+      children.push(series);
     }
 
     const index = Series.new<Index>({type: new Struct(fields), children: children});
 
-    return new DataFrame({[byname]: index, ...rest_map});
+    return new DataFrame({byname: index, ...rest_map});
   }
 
   /**
