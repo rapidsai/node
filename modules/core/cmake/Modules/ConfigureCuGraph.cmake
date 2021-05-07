6534c6e457ca4d3de86d82773b2c50945ec22b57
ptaylor <paul.e.taylor@me.com>
2021-05-07 18:29:24 -0500

    use Robert's cugraph fork

--- modules/core/cmake/Modules/ConfigureCuGraph.cmake
+++ modules/core/cmake/Modules/ConfigureCuGraph.cmake
@@ -18,11 +18,17 @@ function(find_and_configure_cugraph VERSION)
 
     include(get_cpm)
 
+    include(ConfigureCUDF)
+
     _clean_build_dirs_if_not_fully_built(cugraph libcugraph.so)
 
-    _set_package_dir_if_exists(cugraph cugraph)
+    _set_package_dir_if_exists(cuco cuco)
+    _set_package_dir_if_exists(raft raft)
+    _set_package_dir_if_exists(FAISS faiss)
+    # _set_package_dir_if_exists(gunrock gunrock)
+    _set_package_dir_if_exists(cuhornet cuhornet)
 
-    include(ConfigureCUDF)
+    _set_package_dir_if_exists(cugraph cugraph)
 
     if(NOT TARGET cugraph::cugraph)
 
@@ -33,66 +39,28 @@ function(find_and_configure_cugraph VERSION)
         set(BUILD_TESTS OFF)
         set(BUILD_BENCHMARKS OFF)
 
+        set(NCCL_LIBRARY "/usr/local/cuda/lib64/libnccl.so")
+        set(NCCL_INCLUDE_DIR "/usr/local/cuda/include")
+
         CPMFindPackage(NAME     cugraph
             VERSION             ${CUGRAPH_VERSION}
-            GIT_REPOSITORY      https://github.com/rapidsai/cugraph.git
-            GIT_TAG             branch-${CUGRAPH_VERSION}
+            # GIT_REPOSITORY      https://github.com/rapidsai/cugraph.git
+            # GIT_TAG             branch-${CUGRAPH_VERSION}
+            GIT_REPOSITORY      https://github.com/robertmaynard/cugraph.git
+            GIT_TAG             use_rapids_cmake
             GIT_SHALLOW         TRUE
             UPDATE_DISCONNECTED FALSE
-            # SOURCE_SUBDIR  cpp
-            DOWNLOAD_ONLY
+            SOURCE_SUBDIR       cpp
+            OPTIONS             "BUILD_TESTS OFF"
+                                "BUILD_BENCHMARKS OFF"
         )
-
-        # set(CUGRAPH_INCLUDE_DIR_REAL "${cugraph_SOURCE_DIR}/cpp/include")
-        # set(CUGRAPH_INCLUDE_DIR "${cugraph_SOURCE_DIR}/cpp/fake_include")
-        # list(APPEND CUGRAPH_INCLUDE_DIRS ${RAFT_INCLUDE_DIR})
-        # list(APPEND CUGRAPH_INCLUDE_DIRS ${CUGRAPH_INCLUDE_DIR})
-        # list(APPEND CUGRAPH_INCLUDE_DIRS ${CUGRAPH_INCLUDE_DIR_REAL})
-        # list(APPEND CUGRAPH_INCLUDE_DIRS ${cugraph_SOURCE_DIR}/cpp/src)
-        # set(CUGRAPH_INCLUDE_DIRS ${CUGRAPH_INCLUDE_DIRS} PARENT_SCOPE)
-        # set(cugraph_SOURCE_DIR "${cugraph_SOURCE_DIR}" PARENT_SCOPE)
 
         execute_process(COMMAND mkdir -p "${cugraph_SOURCE_DIR}/cpp/fake_include")
         execute_process(COMMAND ln -s -f "${cugraph_SOURCE_DIR}/cpp/include" "${cugraph_SOURCE_DIR}/cpp/fake_include/cugraph")
 
-        # message(STATUS "CUGRAPH_INCLUDE_DIR: ${CUGRAPH_INCLUDE_DIR}")
-        # message(STATUS "CUGRAPH_INCLUDE_DIR_REAL: ${CUGRAPH_INCLUDE_DIR_REAL}")
-        # message(STATUS "CUGRAPH_INCLUDE_DIRS: ${CUGRAPH_INCLUDE_DIRS}")
-
-        # synthesize a cugraph::cugraph target
-        add_library(cugraph SHARED
-            "${cugraph_SOURCE_DIR}/cpp/src/structure/graph.cu"
-            "${cugraph_SOURCE_DIR}/cpp/src/layout/force_atlas2.cu")
-
-        set_target_properties(cugraph
-            PROPERTIES BUILD_RPATH                         "\$ORIGIN"
-                       INSTALL_RPATH                       "\$ORIGIN"
-                       # set target compile options
-                       CXX_STANDARD                        17
-                       CXX_STANDARD_REQUIRED               ON
-                       CUDA_STANDARD                       17
-                       CUDA_STANDARD_REQUIRED              ON
-                       NO_SYSTEM_FROM_IMPORTED             ON
-                       POSITION_INDEPENDENT_CODE           ON
-                       INTERFACE_POSITION_INDEPENDENT_CODE ON
-        )
-
         target_include_directories(cugraph
             PUBLIC "${cugraph_SOURCE_DIR}/cpp/fake_include"
-                   "${cugraph_SOURCE_DIR}/cpp/include"
-                   "${cugraph_SOURCE_DIR}/cpp/src")
-
-        set(NODE_RAPIDS_CUGRAPH_CUDA_FLAGS ${NODE_RAPIDS_CMAKE_CUDA_FLAGS})
-        list(APPEND NODE_RAPIDS_CUGRAPH_CUDA_FLAGS -Xptxas --disable-warnings)
-        list(APPEND NODE_RAPIDS_CUGRAPH_CUDA_FLAGS -Xcompiler=-Wall,-Wno-error=sign-compare,-Wno-error=unused-but-set-variable)
-
-        target_compile_options(cugraph
-            PRIVATE "$<BUILD_INTERFACE:$<$<COMPILE_LANGUAGE:CUDA>:${NODE_RAPIDS_CUGRAPH_CUDA_FLAGS}>>"
-        )
-
-        target_link_libraries(cugraph PUBLIC raft::raft CUDA::cudart_static)
-
-        add_library(cugraph::cugraph ALIAS cugraph)
+                   "${cugraph_SOURCE_DIR}/cpp/include")
     endif()
 endfunction()
 
