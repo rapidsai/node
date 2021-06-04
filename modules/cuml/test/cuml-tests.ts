03fdeb7a1de81b7b1cd9027968b678ed8bab1934
Ajay Thorve <athorve@nvidia.com>
2021-06-03 17:10:47 -0700

    add tests

--- modules/cuml/test/cuml-tests.ts
+++ modules/cuml/test/cuml-tests.ts
@@ -1,1 +1,48 @@
-test('nothing', () => {});
+const {UMAP, trustworthiness, MetricType, CUMLLogLevels} = require('@rapidsai/cuml');
+const {DataFrame}                                        = require('@rapidsai/cudf');
+
+const df = DataFrame.readCSV({
+  header: 0,
+  sourceType: 'files',
+  sources: ['./debug/iris.csv'],
+  dataTypes: {
+    sepal_length: 'float32',
+    sepal_width: 'float32',
+    petal_length: 'float32',
+    petal_width: 'float32',
+    target: 'float32'
+  }
+});
+const X  = df.drop(['target']);
+const y  = df.get('target');
+
+test('fit_transform trustworthiness score', () => {
+  const umap  = new UMAP({
+    nNeighbors: 10,
+    minDist: 0.01,
+    randomState: 12,
+    targetNNeighbors: -1,
+    targetMetric: MetricType.CATEGORICAL
+  });
+  const t1    = umap.fit_transform(X, y, true);
+  const trust = trustworthiness(X, t1, 10);
+
+  expect(trust).toBeGreaterThan(0.97);
+});
+
+test('transform trustworthiness score', () => {
+  const umap = new UMAP({
+    nNeighbors: 10,
+    minDist: 0.01,
+    nEpochs: 800,
+    init: 0,
+    randomState: 42,
+    targetNNeighbors: -1,
+    targetMetric: MetricType.CATEGORICAL
+  });
+  umap.fit(X, y, true);
+  const t1    = umap.transform(X, y, true);
+  const score = trustworthiness(X, t1, 10);
+
+  expect(score).toBeGreaterThan(0.97);
+});
