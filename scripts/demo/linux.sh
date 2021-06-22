e96033f0ea982588b51cded7eb02690fc2e0ca7f
Ajay Thorve <athorve@nvidia.com>
2021-06-21 20:17:36 -0700

    add umap to demos

--- scripts/demo/linux.sh
+++ scripts/demo/linux.sh
@@ -29,7 +29,7 @@ fi
 
 if [[ "$DEMO" == "" ]]; then
     DEMOS="
-    $(echo modules/demo/{graph,luma,spatial,xterm,client-server}/package.json)
+    $(echo modules/demo/{graph,luma,spatial,xterm,client-server,umap}/package.json)
     $(find modules/demo/{deck,tfjs,ipc} -maxdepth 2 -type f -name 'package.json')
     ";
     DEMOS="$(echo -e "$DEMOS" | grep -v node_modules | sort -Vr)";
@@ -55,7 +55,7 @@ if [[ "$DEMO" =~ "modules/demo/luma" ]]; then ARGS="${@:-01}";
 elif [[ "$DEMO" =~ "modules/demo/ipc/umap" ]]; then ARGS="${@:-tcp://0.0.0.0:6000}";
 fi
 
-if [[ "$DEMO" =~ "modules/demo/client-server" ]]; then 
+if [[ "$DEMO" =~ "modules/demo/client-server" ]]; then
     NODE_ENV=production exec npm --prefix="$DEMO" $ARGS start
 else
     NODE_ENV=production exec node -r esm --trace-uncaught "$DEMO" $ARGS
