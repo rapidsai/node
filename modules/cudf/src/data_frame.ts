2a58316d69c21737f4324b2f8d03d670ee8c7872
Bryan Van de Ven <bvandeven@nvidia.com>
2021-06-29 11:41:30 -0700

    Add DataFrame.fromArrow (#250)
    * Add DataFrame.fromArrow
    
    * clean up docstrings
    
    * add generic signature to afford optional user-supplied typing
    
    * Update BINDINGS.md

--- modules/cudf/src/data_frame.ts
+++ modules/cudf/src/data_frame.ts
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {MemoryResource} from '@rapidsai/rmm';
+import {Memory} from '@nvidia/cuda';
+import {DeviceBuffer, MemoryResource} from '@rapidsai/rmm';
 import * as arrow from 'apache-arrow';
 import {compareTypes} from 'apache-arrow/visitor/typecomparator';
 import {Readable} from 'stream';
@@ -111,6 +112,18 @@ export class DataFrame<T extends TypeMap = any> {
     return new DataFrame(new ColumnAccessor(
       names.reduce((map, name, i) => ({...map, [name]: table.getColumnByIndex(i)}),
                    {} as ColumnsMap<{[P in keyof T]: CSVToCUDFType<T[P]>}>)));
+  }
+
+  /**
+   * Adapts an Arrow Table in IPC format into a DataFrame.
+   *
+   * @param memory A buffer holding Arrow table
+   * @return The Arrow data as a DataFrame
+   */
+  public static fromArrow<T extends TypeMap>(memory: Memory|DeviceBuffer): DataFrame<T> {
+    const {names, table} = Table.fromArrow(memory);
+    return new DataFrame(new ColumnAccessor(names.reduce(
+      (map, name, i) => ({...map, [name]: table.getColumnByIndex(i)}), {} as ColumnsMap<T>)));
   }
 
   private _accessor: ColumnAccessor<T>;
