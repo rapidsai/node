f5afc2ae439e9cbb7eec1fa5728407105056d4e7
Lena <lena.hicksonlong@gmail.com>
2021-07-02 14:44:55 -0700

    Remove unneeded comment

--- modules/cudf/src/node_cudf/column.hpp
+++ modules/cudf/src/node_cudf/column.hpp
@@ -653,7 +653,6 @@ struct Column : public EnvLocalObjectWrap<Column> {
     cudf::out_of_bounds_policy bounds_policy = cudf::out_of_bounds_policy::DONT_CHECK,
     rmm::mr::device_memory_resource* mr      = rmm::mr::get_current_device_resource()) const;
 
-  // NOT IMPLEMENTED
   Column::wrapper_t copy(
     rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource()) const;
 
