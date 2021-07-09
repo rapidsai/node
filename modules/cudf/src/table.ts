49a4f487bb874efedd0457ac800793f6f3bab3ff
Bryan Van de Ven <bvandeven@nvidia.com>
2021-07-09 11:01:47 -0700

    missing imports

--- modules/cudf/src/table.ts
+++ modules/cudf/src/table.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import {MemoryData} from '@nvidia/cuda';
-import {MemoryResource} from '@rapidsai/rmm';
+import {DeviceBuffer, MemoryResource} from '@rapidsai/rmm';
 
 import CUDF from './addon';
 import {Column} from './column';
