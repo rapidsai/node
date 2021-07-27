6b774e70b5d999725204ba25dcb07b159063824c
Lena <lena.hicksonlong@gmail.com>
2021-07-27 12:17:41 -0700

    initial test working

--- modules/jsdom/src/index.ts
+++ modules/jsdom/src/index.ts
@@ -11,3 +11,28 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+
+
+
+// Need to bring these over as well
+// new class -> rapidsjsdom
+//    extend the base jsdom class, provide a function that automatically creates the localhost URL
+//    add a before parse thing that will attatch everything to the window
+// First test:
+//    construct an instance of JSDOM
+//    can require file
+//
+//
+
+// imageloader class needs to be moved over
+//
+
+
+//import { JSDOM, ResourceLoader } from 'jsdom';
+import * as jsdom from 'jsdom';
+
+
+export class RapidsJSDOM extends jsdom.JSDOM{
+
+}
--- modules/jsdom/test/jsdom-tests.ts
+++ modules/jsdom/test/jsdom-tests.ts
@@ -12,7 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {RapidsJSDOM} from '@rapidsai/jsdom';
+
 test('nothing', () => {
   // stub test to make sure the debugger works
   debugger;
+});
+
+test('can require file inside customized JSDOM', () => {
+  const {window} = (new RapidsJSDOM()).window;
+  const success  = window.eval((function() {
+    try {
+      var f = require('./local_file');
+      if (f) { return true; }
+    } catch (e) {}
+    return false;
+  })().toString());
+
+  const failure  = window.eval((function() {
+    try {
+      var f = require('./nonexistent_file');
+      if (f) { return true; }
+    } catch (e) {}
+    return false;
+  })().toString());
+
+  expect(success).toBeTruthy();
+  expect(failure).toBeFalsy();
 });
