88a70c5fe8b3fae4679bb031156b98a73a353097
Matthew Zegar <matthewzegar@gmail.com>
2021-07-29 08:47:46 -0700

    Pass in message id to add to cache

--- modules/blazingsql/wrapper/blazingsql/cache.cpp
+++ modules/blazingsql/wrapper/blazingsql/cache.cpp
@@ -37,7 +37,7 @@ void CacheMachine::add_to_cache(std::string const& message_id,
   std::unique_ptr<ral::frame::BlazingTable> table =
     std::make_unique<ral::frame::BlazingTable>(table_view, table_names);
 
-  this->_cache->addToCache(std::move(table), "", true, {}, true);
+  this->_cache->addToCache(std::move(table), message_id, true, {}, true);
 }
 
 std::tuple<std::vector<std::string>, std::unique_ptr<cudf::table>> CacheMachine::pull_from_cache(
