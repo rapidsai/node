96af68dddf91d0064d4074cde244ba8fb3e2a509
Matthew Zegar <matthewzegar@gmail.com>
2021-08-17 14:36:34 -0700

    Remove unused blazingcluster ref in tests

--- modules/blazingsql/test/blazing-context-tests.ts
+++ modules/blazingsql/test/blazing-context-tests.ts
@@ -1,4 +1,4 @@
-import {BlazingCluster, BlazingContext} from '@rapidsai/blazingsql';
+import {BlazingContext} from '@rapidsai/blazingsql';
 import {DataFrame, Float64, Series, Utf8String} from '@rapidsai/cudf';
 
 test('create and drop table', () => {
@@ -11,8 +11,6 @@ test('create and drop table', () => {
 
   bc.dropTable('test_table');
   expect(bc.listTables().length).toEqual(0);
-
-  const test = BlazingCluster.init();
 });
 
 test('list tables', () => {
