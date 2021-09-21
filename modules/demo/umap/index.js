4789536d34f9b6522f98bfa8e930013ec5a4aa09
Ajay Thorve <athorve@nvidia.com>
2021-09-21 15:10:25 -0700

    update demo to accomadate the recent changes

--- modules/demo/umap/index.js
+++ modules/demo/umap/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env -S node -r esm
-
 // Copyright (c) 2020-2021, NVIDIA CORPORATION.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,50 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-require('segfault-handler').registerHandler('./crash.log');
+module.exports = (glfwOptions = {
+  title: '',
+  visible: true,
+  transparent: false,
+}) => {
+  let args = process.argv.slice(2);
+  if (args.length === 1 && args[0].includes(' ')) { args = args[0].split(' '); }
 
-require('@babel/register')({
-  cache: false,
-  babelrc: false,
-  cwd: __dirname,
-  presets: [
-    ['@babel/preset-env', { 'targets': { 'node': 'current' } }],
-    ['@babel/preset-react', { 'useBuiltIns': true }]
-  ]
-});
-
-// Change cwd to the example dir so relative file paths are resolved
-process.chdir(__dirname);
-
-let args = process.argv.slice(2);
-if (args.length === 1 && args[0].includes(' ')) { args = args[0].split(' '); }
-
-module.exports = require('@nvidia/glfw').createReactWindow(`${__dirname}/src/index.js`, true);
-
-if (require.main === module) {
   const parseArg = (prefix, fallback = '') =>
     (args.find((arg) => arg.includes(prefix)) || `${prefix}${fallback}`).slice(prefix.length);
 
   const delay = Math.max(0, parseInt(parseArg('--delay=', 0)) | 0);
 
-  module.exports.open({
-    visible: true,
-    transparent: false,
-    _title: '',
-    nodes: inputs(delay, parseArg('--nodes=')),
-    edges: inputs(delay, parseArg('--edges=')),
-    width: parseInt(parseArg('--width=', 800)) | 0,
-    height: parseInt(parseArg('--height=', 600)) | 0,
-    layoutParams: JSON.parse(`{${parseArg('--params=')}}`),
-  });
-
   async function* inputs(delay, paths) {
     const sleep = (t) => new Promise((r) => setTimeout(r, t));
     for (const path of paths.split(',')) {
-      if (path) {
-        yield path;
-      }
+      if (path) { yield path; }
       await sleep(delay);
     }
   }
+
+  return require('@rapidsai/jsdom')
+    .RapidsJSDOM.fromReactComponent(
+      './src/app.jsx',
+      {
+        glfwOptions,
+        // Change cwd to the example dir so relative file paths are resolved
+        module: {path: __dirname},
+      },
+      {
+        nodes: inputs(delay, parseArg('--nodes=')),
+        edges: inputs(delay, parseArg('--edges=')),
+        width: parseInt(parseArg('--width=', 800)) | 0,
+        height: parseInt(parseArg('--height=', 600)) | 0,
+        layoutParams: JSON.parse(`{${parseArg('--params=')}}`),
+      });
+};
+
+if (require.main === module) {
+  module.exports().window.addEventListener('close', () => process.exit(0), {once: true});
 }
