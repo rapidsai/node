fbed8e3e6f34e1525a7ece5b89480f7d91e5b7cb
Matthew Zegar <matthewzegar@gmail.com>
2021-10-12 10:34:03 -0700

    WIP tab list

--- modules/demo/viz-app/pages/demo/umap.jsx
+++ modules/demo/viz-app/pages/demo/umap.jsx
@@ -12,26 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import React from 'react';
 import DemoDashboard from "../../components/demo-dashboard/demo-dashboard";
 
 export default class UMAP extends React.Component {
+  constructor(props) {
+    super(props);
+    this.dataTable = this.dataTable.bind(this);
+    this.dataMetrics = this.dataMetrics.bind(this);
+  }
+
+  dataTable() {
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Node List</Tab>
+          <Tab>Edge List</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div>This is node list</div>
+        </TabPanel>
+        <TabPanel>
+          <div>This is edge list</div>
+        </TabPanel>
+      </Tabs>
+    )
+  }
+
+  dataMetrics() {
+    return <div>Data Metrics</div>
+  }
+
   render() {
     return (
       <DemoDashboard demoName={"UMAP Demo"}
         onLoadClick={(fileName) => { console.log(fileName) }}
         onRenderClick={() => { console.log("Render Clicked") }}
-        dataTable={dataTable()}
-        dataMetrics={dataMetrics()}
+        dataTable={this.dataTable()}
+        dataMetrics={this.dataMetrics()}
       />
     )
   }
-}
-
-function dataTable() {
-  return <div>Dable Table</div>
-}
-
-function dataMetrics() {
-  return <div>Data Metrics</div>
 }
