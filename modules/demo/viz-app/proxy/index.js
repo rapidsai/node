5dc3edad6a228f918fb7768bcf3755f8b210aec5
Ajay Thorve <athorve@nvidia.com>
2021-10-22 13:09:27 -0700

    change front-end app port

--- modules/demo/viz-app/proxy/index.js
+++ modules/demo/viz-app/proxy/index.js
@@ -1,3 +1,3 @@
 var proxy = require('redbird')({port: 3000, bunyan: false});
-proxy.register('http://localhost:3000', 'http://localhost:3001/');
+proxy.register('http://localhost:3000', 'http://localhost:3005/');
 proxy.register('http://localhost:3000/api', 'http://localhost:8080/');
