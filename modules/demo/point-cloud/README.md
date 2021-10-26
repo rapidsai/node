7f15dad7f16d56f68f4e678ad356935bcfa4c2ea
Matthew Zegar <matthewzegar@gmail.com>
2021-10-26 15:04:44 -0700

    Setup DeckGL point-cloud demo

---
+++ modules/demo/point-cloud/README.md
@@ -0,0 +1,21 @@
+# Point-Cloud Demo
+
+## Main Dependencies
+
+1. deckgl
+
+## Installation
+
+To install dependencies, run the following from the root directory for `node-rapids`
+
+```bash
+yarn
+```
+
+To run the demo
+```bash
+yarn demo #and select the point-cloud demo from the list of demos
+
+cd modules/demo/point-cloud
+yarn start #yarn dev for dev environment
+```
---
+++ modules/demo/point-cloud/components/temp-deckgl.jsx
@@ -0,0 +1,42 @@
+// Copyright (c) 2021, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { LineLayer } from '@deck.gl/layers';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+  { sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781] }
+];
+
+// DeckGL react component
+export default function TempDeckGL() {
+  const layers = [
+    new LineLayer({ id: 'line-layer', data })
+  ];
+
+  return <DeckGL
+    initialViewState={INITIAL_VIEW_STATE}
+    controller={true}
+    layers={layers} />;
+}
