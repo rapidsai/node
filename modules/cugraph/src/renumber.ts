4675e61080d0a87aae6d673fbebb6f823f357ca3
Bryan Van de Ven <bvandeven@nvidia.com>
2021-11-01 16:16:25 -0700

    make renum funcs more generic

--- modules/cugraph/src/renumber.ts
+++ modules/cugraph/src/renumber.ts
@@ -12,7 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {DataFrame, Int32, Series, StringSeries, Utf8String} from '@rapidsai/cudf';
+import {
+  DataFrame,
+  DataType,
+  Int32,
+  ListSeries,
+  Numeric,
+  Series,
+  StructSeries,
+  TypeMap,
+  Utf8String,
+} from '@rapidsai/cudf';
 
 function join(edges: DataFrame, nodes: DataFrame<{id: Int32, node: Utf8String}>, col: string) {
   const tmp = edges.assign({node: edges.get(col)}).join({other: nodes, on: ['node']}).sortValues({
@@ -21,14 +31,31 @@ function join(edges: DataFrame, nodes: DataFrame<{id: Int32, node: Utf8String}>,
   return edges.drop([col]).assign({[col]: tmp.get('id')});
 }
 
-export function renumber_nodes(src: StringSeries, dst: StringSeries) {
-  const nodes = src.concat(dst).unique() as StringSeries;
+export function renumber_nodes<T extends Series<Numeric>>(src: T, dst: T): DataFrame;
+export function renumber_nodes(src: Series<Utf8String>, dst: Series<Utf8String>): DataFrame;
+export function renumber_nodes<R extends DataType, T extends ListSeries<R>>(src: T,
+                                                                            dst: T): DataFrame;
+export function renumber_nodes<R extends TypeMap, T extends StructSeries<R>>(src: T,
+                                                                             dst: T): DataFrame;
+
+export function renumber_nodes(src: any, dst: any): DataFrame {
+  const nodes = src.concat(dst).unique();
   const ids   = nodes.encodeLabels();
   return new DataFrame({'id': ids, 'node': nodes}).sortValues({id: {ascending: true}});
 }
 
+export function renumber_edges<T extends Series<Numeric>>(
+  src: T, dst: T, nodes: DataFrame<{id: Int32, node: Utf8String}>): DataFrame;
+export function renumber_edges(src: Series<Utf8String>,
+                               dst: Series<Utf8String>,
+                               nodes: DataFrame<{id: Int32, node: Utf8String}>): DataFrame;
+export function renumber_edges<R extends DataType, T extends ListSeries<R>>(
+  src: T, dst: T, nodes: DataFrame<{id: Int32, node: Utf8String}>): DataFrame;
+export function renumber_edges<R extends TypeMap, T extends StructSeries<R>>(
+  src: T, dst: T, nodes: DataFrame<{id: Int32, node: Utf8String}>): DataFrame;
+
 export function renumber_edges(
-  src: StringSeries, dst: StringSeries, nodes: DataFrame<{id: Int32, node: Utf8String}>) {
+  src: any, dst: any, nodes: DataFrame<{id: Int32, node: Utf8String}>): DataFrame {
   const idx   = Series.sequence({type: new Int32, size: src.length, init: 0});
   const edges = new DataFrame({src: src, dst: dst, idx: idx});
   return join(join(edges, nodes, 'src'), nodes, 'dst').sortValues({idx: {ascending: true}}).drop([
