f810247ab71e284600d9c9708981a90f1b0af728
Matthew Zegar <matthewzegar@gmail.com>
2021-11-10 13:38:47 -0800

    Parse laz header

---
+++ modules/io/src/laz.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2021, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <laz.hpp>
+
+std::unique_ptr<cudf::io::datasource::buffer> Laz::read_bytes(const size_t& size) {
+  auto result = _datasource->host_read(this->_byte_offset, size);
+  this->_byte_offset += size;
+  return result;
+}
+
+void Laz::parse_header() {
+  auto file_signature          = this->read_bytes(4);
+  this->_header.file_signature = *file_signature->data();
+
+  auto file_source_id          = this->read_bytes(2);
+  this->_header.file_source_id = *file_source_id->data();
+
+  auto global_encoding          = this->read_bytes(2);
+  this->_header.global_encoding = *global_encoding->data();
+
+  // Skip Project ID
+  this->read_bytes(16);
+
+  auto version_major          = this->read_bytes(1);
+  this->_header.version_major = *version_major->data();
+
+  auto version_minor          = this->read_bytes(1);
+  this->_header.version_minor = *version_minor->data();
+
+  auto system_identifier          = this->read_bytes(32);
+  this->_header.system_identifier = *system_identifier->data();
+
+  auto generating_software          = this->read_bytes(32);
+  this->_header.generating_software = *generating_software->data();
+
+  // Skip file creation
+  this->read_bytes(4);
+
+  auto header_size          = this->read_bytes(2);
+  this->_header.header_size = *header_size->data();
+
+  auto point_data_offset          = this->read_bytes(4);
+  this->_header.point_data_offset = *point_data_offset->data();
+
+  auto variable_length_records_count          = this->read_bytes(4);
+  this->_header.variable_length_records_count = *variable_length_records_count->data();
+
+  auto point_data_format_id          = this->read_bytes(1);
+  this->_header.point_data_format_id = *point_data_format_id->data();
+
+  auto point_data_record_length          = this->read_bytes(4);
+  this->_header.point_data_record_length = *point_data_record_length->data();
+
+  auto point_record_count          = this->read_bytes(4);
+  this->_header.point_record_count = *point_record_count->data();
+
+  // TODO
+  // unsigned long[5]
+  // number of points by return
+  this->read_bytes(20);
+
+  auto x_scale          = this->read_bytes(8);
+  this->_header.x_scale = *x_scale->data();
+
+  auto y_scale          = this->read_bytes(8);
+  this->_header.y_scale = *y_scale->data();
+
+  auto z_scale          = this->read_bytes(8);
+  this->_header.z_scale = *z_scale->data();
+
+  auto x_offset          = this->read_bytes(8);
+  this->_header.x_offset = *x_offset->data();
+
+  auto y_offset          = this->read_bytes(8);
+  this->_header.y_offset = *y_offset->data();
+
+  auto z_offset          = this->read_bytes(8);
+  this->_header.z_offset = *z_offset->data();
+
+  auto max_x          = this->read_bytes(8);
+  this->_header.max_x = *max_x->data();
+
+  auto min_x          = this->read_bytes(8);
+  this->_header.min_x = *min_x->data();
+
+  auto max_y          = this->read_bytes(8);
+  this->_header.max_y = *max_y->data();
+
+  auto min_y          = this->read_bytes(8);
+  this->_header.min_y = *min_y->data();
+
+  auto max_z          = this->read_bytes(8);
+  this->_header.max_z = *max_z->data();
+
+  auto min_z          = this->read_bytes(8);
+  this->_header.min_z = *min_z->data();
+}
---
+++ modules/io/src/laz.hpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2021, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cudf/io/datasource.hpp>
+
+struct LazHeader {
+  std::string file_signature;
+  unsigned short file_source_id;
+  unsigned short global_encoding;
+  unsigned char version_major;
+  unsigned char version_minor;
+  std::string system_identifier;
+  std::string generating_software;
+  unsigned short header_size;
+  unsigned long point_data_offset;
+  unsigned long variable_length_records_count;
+  unsigned char point_data_format_id;
+  unsigned short point_data_record_length;
+  unsigned long point_record_count;
+  unsigned long points_by_return_count[5];
+  double x_scale;
+  double y_scale;
+  double z_scale;
+  double x_offset;
+  double y_offset;
+  double z_offset;
+  double max_x;
+  double min_x;
+  double max_y;
+  double min_y;
+  double max_z;
+  double min_z;
+};
+
+class Laz {
+ public:
+  Laz(const std::string& path) {
+    _datasource  = ::cudf::io::datasource::create(path);
+    _byte_offset = 0;
+
+    parse_header();
+  }
+
+ private:
+  std::unique_ptr<cudf::io::datasource> _datasource;
+  size_t _byte_offset;
+
+  LazHeader _header;
+
+  std::unique_ptr<cudf::io::datasource::buffer> read_bytes(const size_t& size);
+  void parse_header();
+};
