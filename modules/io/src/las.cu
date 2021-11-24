59381c3763fdaa589c3653c7b995c579bc43206d
Matthew Zegar <matthewzegar@gmail.com>
2021-11-24 11:14:18 -0800

    Add formating for all point types

--- modules/io/src/las.cu
+++ modules/io/src/las.cu
@@ -234,78 +234,242 @@ __global__ void parse_variable_length_header(uint8_t const* las_variable_header_
   byte_offset += 32;
 }
 
+__device__ void parse_point_record_format_0(uint8_t const* point_data,
+                                            LasHeader* header_data,
+                                            PointRecord* result) {
+  for (size_t i = 0; i < header_data->point_record_count; ++i) {
+    size_t byte_offset = i * header_data->point_data_size;
+
+    // x (4 bytes)
+    result->x = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // y (4 bytes)
+    result->y = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // z (4 bytes)
+    result->z = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // intensity (2 bytes)
+    // not required
+    byte_offset += 2;
+
+    // return number (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // classification (1 byte)
+    result->classification = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // Scan angle (1 byte)
+    result->scan_angle = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // User data (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // Point source id (2 bytes)
+    result->point_source_id = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+  }
+}
+
+__device__ void parse_point_record_format_1(uint8_t const* point_data,
+                                            LasHeader* header_data,
+                                            PointRecord* result) {
+  for (size_t i = 0; i < header_data->point_record_count; ++i) {
+    size_t byte_offset = i * header_data->point_data_size;
+
+    // x (4 bytes)
+    result->x = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // y (4 bytes)
+    result->y = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // z (4 bytes)
+    result->z = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // intensity (2 bytes)
+    // not required
+    byte_offset += 2;
+
+    // return number (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // classification (1 byte)
+    result->classification = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // Scan angle (1 byte)
+    result->scan_angle = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // User data (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // Point source id (2 bytes)
+    result->point_source_id = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // GPS time (8 bytes)
+    result->gps_time =
+      *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+      *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24 |
+      *(point_data + byte_offset + 4) << 32 | *(point_data + byte_offset + 5) << 40 |
+      *(point_data + byte_offset + 6) << 48 | *(point_data + byte_offset + 7) << 56;
+    byte_offset += 8;
+  }
+}
+
+__device__ void parse_point_record_format_2(uint8_t const* point_data,
+                                            LasHeader* header_data,
+                                            PointRecord* result) {
+  for (size_t i = 0; i < header_data->point_record_count; ++i) {
+    size_t byte_offset = i * header_data->point_data_size;
+
+    // x (4 bytes)
+    result->x = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // y (4 bytes)
+    result->y = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // z (4 bytes)
+    result->z = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // intensity (2 bytes)
+    // not required
+    byte_offset += 2;
+
+    // return number (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // classification (1 byte)
+    result->classification = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // Scan angle (1 byte)
+    result->scan_angle = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // User data (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // Point source id (2 bytes)
+    result->point_source_id = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // Red (2 bytes)
+    result->red = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // Green (2 bytes)
+    result->green = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // Blue (2 bytes)
+    result->blue = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+  }
+}
+
+__device__ void parse_point_record_format_3(uint8_t const* point_data,
+                                            LasHeader* header_data,
+                                            PointRecord* result) {
+  for (size_t i = 0; i < header_data->point_record_count; ++i) {
+    size_t byte_offset = i * header_data->point_data_size;
+
+    // x (4 bytes)
+    result->x = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // y (4 bytes)
+    result->y = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // z (4 bytes)
+    result->z = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+                *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
+    byte_offset += 4;
+
+    // intensity (2 bytes)
+    // not required
+    byte_offset += 2;
+
+    // return number (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // classification (1 byte)
+    result->classification = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // Scan angle (1 byte)
+    result->scan_angle = *(point_data + byte_offset);
+    byte_offset += 1;
+
+    // User data (1 byte)
+    // not required
+    byte_offset += 1;
+
+    // Point source id (2 bytes)
+    result->point_source_id = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // GPS time (8 bytes)
+    result->gps_time =
+      *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
+      *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24 |
+      *(point_data + byte_offset + 4) << 32 | *(point_data + byte_offset + 5) << 40 |
+      *(point_data + byte_offset + 6) << 48 | *(point_data + byte_offset + 7) << 56;
+    byte_offset += 8;
+
+    // Red (2 bytes)
+    result->red = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // Green (2 bytes)
+    result->green = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+
+    // Blue (2 bytes)
+    result->blue = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
+    byte_offset += 2;
+  }
+}
+
 __global__ void parse_point_record(uint8_t const* point_data,
                                    LasHeader* header_data,
                                    PointRecord* result) {
   switch (header_data->point_data_format_id) {
-    case 0: break;  // format 0
-    case 1: break;  // format 1
-    case 2: break;  // format 2
-    case 3:         // format 3
-      for (size_t i = 0; i < header_data->point_record_count; ++i) {
-        size_t byte_offset = i * header_data->point_data_size;
-
-        // x (4 bytes)
-        result->x = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
-                    *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
-        byte_offset += 4;
-
-        // y (4 bytes)
-        result->y = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
-                    *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
-        byte_offset += 4;
-
-        // z (4 bytes)
-        result->z = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
-                    *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24;
-        byte_offset += 4;
-
-        // intensity (2 bytes)
-        // not required
-        byte_offset += 2;
-
-        // return number (1 byte)
-        // not required
-        byte_offset += 1;
-
-        // classification (1 byte)
-        result->classification = *(point_data + byte_offset);
-        byte_offset += 1;
-
-        // Scan angle (1 byte)
-        result->scan_angle = *(point_data + byte_offset);
-        byte_offset += 1;
-
-        // User data (1 byte)
-        // not required
-        byte_offset += 1;
-
-        // Point source id (2 bytes)
-        result->point_source_id = *(point_data + byte_offset) | *(point_data + byte_offset + 1)
-                                                                  << 8;
-        byte_offset += 2;
-
-        // GPS time (8 bytes)
-        result->gps_time =
-          *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8 |
-          *(point_data + byte_offset + 2) << 16 | *(point_data + byte_offset + 3) << 24 |
-          *(point_data + byte_offset + 4) << 32 | *(point_data + byte_offset + 5) << 40 |
-          *(point_data + byte_offset + 6) << 48 | *(point_data + byte_offset + 7) << 56;
-        byte_offset += 8;
-
-        // Red (2 bytes)
-        result->red = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
-        byte_offset += 2;
-
-        // Green (2 bytes)
-        result->green = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
-        byte_offset += 2;
-
-        // Blue (2 bytes)
-        result->blue = *(point_data + byte_offset) | *(point_data + byte_offset + 1) << 8;
-        byte_offset += 2;
-      }
-      break;
+    case 0: parse_point_record_format_0(point_data, header_data, result); break;  // format 0
+    case 1: parse_point_record_format_1(point_data, header_data, result); break;  // format 1
+    case 2: parse_point_record_format_2(point_data, header_data, result); break;  // format 2
+    case 3: parse_point_record_format_3(point_data, header_data, result); break;  // format 3
   }
 }
 
