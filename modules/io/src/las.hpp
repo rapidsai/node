eeaac006269567870050b034bc342d096c63512e
Matthew Zegar <32153021+mzegar@users.noreply.github.com>
2021-12-15 09:40:10 -0800

    [FEA] IO Module (#329)
    * Basic setup of IO module
    
    * Fix CPP compile, add basic test
    
    * Add in io module to containers
    
    * Export testing method
    
    * Fix import test
    
    * Fix io module resolve
    
    * Add missing license, update README
    
    * Link thrust
    
    * Add and compile .cu files
    
    * Link lubcudf
    
    * Stub in test thrust method
    
    * WIP test file and env
    
    * Get an understanding of laz header parsing
    
    * Parse laz header
    
    * Parse variable header
    
    * Move constructor implementation to cpp file, call CUDA file
    
    * Swap if device read is not available
    
    * Copy host memory to device buffer
    
    * Parse header to device
    
    * Call into CUDA header parse
    
    * Create laz struct on GPU
    
    * Parse out laz header on GPU
    
    * Parse variable header on GPU
    
    * Properly combine values
    
    * Update variable header with proper bit shift
    
    * Setup initial point cloud parsing
    
    * Extract data from point data id
    
    * Revert back to unsigned char
    
    * Parse out point format 3
    
    * Attempt proper parse of data
    
    * Use .laz file instead of .laz
    
    * Cleanup parse methods
    
    * Allocate enough memory for array of structs
    
    * Add formating for all point types
    
    * Populate array of structs, copy over data to host
    
    * Use cudaOccupancyMaxPotentialBlockSize
    
    * Fix other point cloud for loops
    
    * Add pauls changes, device lambda
    
    * Get compiling
    
    * Fix memory error
    
    * Semi working point format 3 working
    
    * complete point format 3
    
    * Attempt to parse most format 3
    
    * Pass in most point format params
    
    * Minor rename
    
    * Implement other point formats
    
    * Store header in las class
    
    * Minor cleanup
    
    * Remove las class, just return table
    
    * Return back a dataframe
    
    * Minor rename
    
    * Pass in device memory
    
    * Give names to table
    
    * Update lock file

---
+++ modules/io/src/las.hpp
@@ -0,0 +1,173 @@
+// Copyright (c) 2021, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <cudf/io/datasource.hpp>
+#include <cudf/table/table.hpp>
+#include <rmm/device_buffer.hpp>
+
+const int HEADER_BYTE_SIZE = 227;
+
+struct LasHeader {
+  char file_signature[4];
+  unsigned short file_source_id;
+  unsigned short global_encoding;
+  unsigned char version_major;
+  unsigned char version_minor;
+  char system_identifier[32];
+  char generating_software[32];
+  unsigned short header_size;
+  unsigned long point_data_offset;
+  unsigned long variable_length_records_count;
+  unsigned char point_data_format_id;
+  unsigned short point_data_size;
+  unsigned long point_record_count;
+  unsigned long points_by_return_count[5];
+  double x_scale;
+  double y_scale;
+  double z_scale;
+  double x_offset;
+  double y_offset;
+  double z_offset;
+  double max_x;
+  double min_x;
+  double max_y;
+  double min_y;
+  double max_z;
+  double min_z;
+};
+
+const std::vector<std::string> PointDataFormatZeroColumnNames = {"x",
+                                                                 "y",
+                                                                 "z",
+                                                                 "intensity",
+                                                                 "bit_data",
+                                                                 "classification",
+                                                                 "scan_angle",
+                                                                 "user_data",
+                                                                 "point_source_id"};
+
+struct PointDataFormatZero {
+  long x;
+  long y;
+  long z;
+  unsigned short intensity;
+  unsigned char bit_data;
+  unsigned char classification;
+  char scan_angle;
+  unsigned char user_data;
+  unsigned short point_source_id;
+};
+
+const std::vector<std::string> PointDataFormatOneColumnNames = {"x",
+                                                                "y",
+                                                                "z",
+                                                                "intensity",
+                                                                "bit_data",
+                                                                "classification",
+                                                                "scan_angle",
+                                                                "user_data",
+                                                                "point_source_id",
+                                                                "gps_time"};
+
+struct PointDataFormatOne {
+  long x;
+  long y;
+  long z;
+  unsigned short intensity;
+  unsigned char bit_data;
+  unsigned char classification;
+  char scan_angle;
+  unsigned char user_data;
+  unsigned short point_source_id;
+  double gps_time;
+};
+
+const std::vector<std::string> PointDataFormatTwoColumnNames = {"x",
+                                                                "y",
+                                                                "z",
+                                                                "intensity",
+                                                                "bit_data",
+                                                                "classification",
+                                                                "scan_angle",
+                                                                "user_data",
+                                                                "point_source_id",
+                                                                "red",
+                                                                "green",
+                                                                "blue"};
+
+struct PointDataFormatTwo {
+  long x;
+  long y;
+  long z;
+  unsigned short intensity;
+  unsigned char bit_data;
+  unsigned char classification;
+  char scan_angle;
+  unsigned char user_data;
+  unsigned short point_source_id;
+  unsigned short red;
+  unsigned short green;
+  unsigned short blue;
+};
+
+const std::vector<std::string> PointDataFormatThreeColumnNames = {"x",
+                                                                  "y",
+                                                                  "z",
+                                                                  "intensity",
+                                                                  "bit_data",
+                                                                  "classification",
+                                                                  "scan_angle",
+                                                                  "user_data",
+                                                                  "point_source_id",
+                                                                  "gps_time"};
+
+struct PointDataFormatThree {
+  long x;
+  long y;
+  long z;
+  unsigned short intensity;
+  unsigned char bit_data;
+  unsigned char classification;
+  char scan_angle;
+  unsigned char user_data;
+  unsigned short point_source_id;
+  double gps_time;
+  unsigned short red;
+  unsigned short green;
+  unsigned short blue;
+};
+
+std::unique_ptr<cudf::io::datasource::buffer> read(
+  const std::unique_ptr<cudf::io::datasource>& datasource,
+  size_t offset,
+  size_t size,
+  rmm::cuda_stream_view stream);
+
+std::unique_ptr<cudf::table> get_point_cloud_records(
+  const std::unique_ptr<cudf::io::datasource>& datasource,
+  LasHeader* cpu_header,
+  rmm::mr::device_memory_resource* mr,
+  rmm::cuda_stream_view stream);
+
+void parse_las_header_host(const std::unique_ptr<cudf::io::datasource>& datasource,
+                           LasHeader* cpu_header,
+                           LasHeader* gpu_header,
+                           rmm::cuda_stream_view stream);
+
+std::tuple<std::vector<std::string>, std::unique_ptr<cudf::table>> parse_las_host(
+  const std::unique_ptr<cudf::io::datasource>& datasource,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource(),
+  rmm::cuda_stream_view stream        = rmm::cuda_stream_default);
