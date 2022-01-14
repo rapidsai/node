b73f8bfc9fd415d28847d0e809a4d9bbd2d87294
Bryan Van de Ven <bvandeven@nvidia.com>
2022-01-13 19:35:19 -0800

    Fix typedoc issues (#346)
    * fix typedoc issues
    
    * remove test dirs from tsconfig include

--- typedoc.js
+++ typedoc.js
@@ -1,30 +1,22 @@
-const Path        = require('path');
-const loadOptions = (base) => {
-  const opts       = require(Path.resolve(base, 'typedoc.js'));
-  opts.entryPoints = opts.entryPoints.map((entryPoint) => Path.resolve(base, entryPoint));
-  return opts;
+module.exports = {
+  entryPoints: [
+    'modules/cuda/src/index.ts',
+    'modules/rmm/src/index.ts',
+    'modules/cudf/src/index.ts',
+    'modules/cuml/src/index.ts',
+    'modules/cugraph/src/index.ts',
+    'modules/cuspatial/src/index.ts',
+    'modules/deck.gl/src/index.ts',
+    'modules/glfw/src/index.ts',
+    'modules/webgl/src/index.ts',
+    'modules/sql/src/index.ts',
+    'modules/io/src/index.ts'
+  ],
+  out: 'doc',
+  name: 'RAPIDS',
+  tsconfig: 'tsconfig.json',
+  hideGenerator: true,
+  excludePrivate: true,
+  excludeProtected: true,
+  excludeExternals: true,
 };
-
-const mergeOptions = (...opts) => Object.assign(
-  {}, ...opts, {entryPoints: opts.reduce((es, {entryPoints = []}) => [...es, ...entryPoints], [])});
-
-module.exports = mergeOptions(loadOptions('modules/cuda'),
-                              loadOptions('modules/rmm'),
-                              loadOptions('modules/cudf'),
-                              loadOptions('modules/cuml'),
-                              loadOptions('modules/cugraph'),
-                              loadOptions('modules/cuspatial'),
-                              loadOptions('modules/deck.gl'),
-                              loadOptions('modules/glfw'),
-                              loadOptions('modules/webgl'),
-                              loadOptions('modules/sql'),
-                              loadOptions('modules/io'),
-                              {
-                                out: 'doc',
-                                name: 'RAPIDS',
-                                tsconfig: 'tsconfig.json',
-                                hideGenerator: true,
-                                excludePrivate: true,
-                                excludeProtected: true,
-                                excludeExternals: true,
-                              });
