8753a9505be405ff64de222f1be669946371d179
ptaylor <paul.e.taylor@me.com>
2022-01-20 09:51:20 -0800

    fix merge conflict

--- typedoc.js
+++ typedoc.js
@@ -6,11 +6,12 @@ module.exports = {
     'modules/cuml/src/index.ts',
     'modules/cugraph/src/index.ts',
     'modules/cuspatial/src/index.ts',
+    'modules/io/src/index.ts',
     'modules/deck.gl/src/index.ts',
     'modules/glfw/src/index.ts',
+    'modules/jsdom/src/index.ts',
     'modules/webgl/src/index.ts',
     'modules/sql/src/index.ts',
-    'modules/io/src/index.ts'
   ],
   out: 'doc',
   name: 'RAPIDS',
@@ -20,30 +21,3 @@ module.exports = {
   excludeProtected: true,
   excludeExternals: true,
 };
-<<<<<<< HEAD
-=======
-
-const mergeOptions = (...opts) => Object.assign(
-  {}, ...opts, {entryPoints: opts.reduce((es, {entryPoints = []}) => [...es, ...entryPoints], [])});
-
-module.exports = mergeOptions(loadOptions('modules/cuda'),
-                              loadOptions('modules/rmm'),
-                              loadOptions('modules/cudf'),
-                              loadOptions('modules/cuml'),
-                              loadOptions('modules/cugraph'),
-                              loadOptions('modules/cuspatial'),
-                              loadOptions('modules/deck.gl'),
-                              loadOptions('modules/glfw'),
-                              loadOptions('modules/webgl'),
-                              loadOptions('modules/sql'),
-                              loadOptions('modules/io'),
-                              {
-                                out: 'doc',
-                                name: 'RAPIDS',
-                                tsconfig: 'tsconfig.json',
-                                hideGenerator: true,
-                                excludePrivate: true,
-                                excludeProtected: true,
-                                excludeExternals: true,
-                              });
->>>>>>> eeaac006 ([FEA] IO Module (#329))
