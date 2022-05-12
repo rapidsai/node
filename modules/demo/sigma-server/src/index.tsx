628f2214e6c610a3d0359f281133af6c616b22d2
H. Thomson Comer <thomcom@gmail.com>
2022-05-12 17:09:31 -0500

    Commit with sigma-server directory and rapids-api-server directories and working wireframes for them.

---
+++ modules/demo/sigma-server/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import Root from "./views/Root";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
