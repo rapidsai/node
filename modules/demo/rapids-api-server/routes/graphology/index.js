8c568682deb9aa5973677beb2da54cb5d69bb315
H. Thomson Comer <thomcom@gmail.com>
2022-05-17 13:37:03 -0500

    First pass with fully working fastify server.

---
+++ modules/demo/rapids-api-server/routes/graphology/index.js
@@ -0,0 +1,124 @@
+// Copyright (c) 2022, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const Fs                                                            = require('fs');
+const {Utf8String, Int32, DataFrame, StringSeries, Series, Float64} = require('@rapidsai/cudf');
+const {Field, Vector, List}                                         = require('apache-arrow');
+const Path                                                          = require('path');
+const {promisify}                                                   = require('util');
+const Stat                                                          = promisify(Fs.stat);
+
+const fastify   = require('fastify');
+const gpu_cache = require('../../util/gpu_cache.js');
+
+module.exports = async function(fastify, opts) {
+  fastify.get('/', async function(request, reply) {
+    return {
+      graphology: {
+        description: 'The graphology api provides GPU acceleration of graphology datasets.',
+        schema: {
+          read_json: {
+            filename: 'A URI to a graphology json dataset file.',
+            returns: 'Result OK/Not Found/Fail'
+          },
+          list_tables:
+            {returns: 'An object containing graphology related datasets resident on GPU memory.'},
+
+          ':table': {
+            ':column': 'The name of the column you want to request.',
+            returns: 'An arrow buffer of the column contents.'
+          }
+        }
+      }
+    }
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/read_json',
+    schema: {
+      querystring: {filename: {type: 'string'}, 'rootkeys': {type: 'array'}},
+
+      response: {
+        200: {
+          type: 'object',
+          properties:
+            {success: {type: 'boolean'}, message: {type: 'string'}, params: {type: 'string'}}
+        }
+      }
+    },
+    handler: async (request, reply) => {
+      // load the file via read_text
+      // is the file local or remote?
+      let message = 'Unknown error';
+      let result  = {'params': JSON.stringify(request.query), success: false, message: message};
+
+      console.log(result);
+      if (request.query.filename.search('http') != -1) {
+        message = 'Remote files not supported yet.'
+        console.log(result);
+      } else {
+        message = 'File is not remote';
+        console.log(message);
+        // does the file exist?
+        const path = Path.join(__dirname, request.query.filename);
+        console.log('Does the file exist?');
+        try {
+          const stats = await Stat(path);
+          if (stats == null) {
+            message = 'File does not exist at ' + path;
+            console.log(message);
+            result.message = message;
+            reply.code(200).send(result);
+          } else {
+            // did the file read?
+            message = 'File is available';
+            console.log(message);
+            result.success   = true;
+            message          = 'Successfully parsed json file onto GPU.';
+            result.message   = message;
+            const graphology = gpu_cache.readGraphology(path);
+            gpu_cache.setDataframe('nodes', graphology['nodes']);
+            gpu_cache.setDataframe('edges', graphology['edges']);
+            gpu_cache.setDataframe('clusters', graphology['clusters']);
+            gpu_cache.setDataframe('tags', graphology['tags']);
+            reply.code(200).send(result);
+          }
+        } catch {
+          message        = 'Exception reading file.';
+          result.message = message;
+          reply.code(200).send(result);
+        };
+      }
+    }
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: '/get_column/:table/:column',
+    schema: {querystring: {table: {type: 'string'}, 'column': {type: 'string'}}},
+    handler: async (request, reply) => {
+      let message = 'Not Implemented';
+      let result  = {'params': JSON.stringify(request.params), success: false, message: message};
+      console.log(request.params.table);
+      const table = gpu_cache.getDataframe(request.params.table);
+      console.log(table);
+      const column = table.get(request.params.column);
+      console.log(column);
+      console.log(result);
+      result.column = column.toArray();
+      reply.code(200).send(result);
+    }
+  });
+}
---
+++ modules/demo/rapids-api-server/util/gpu_cache.js
@@ -0,0 +1,91 @@
+// Copyright (c) 2022, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const {Utf8String, Int32, DataFrame, StringSeries, Series,  Float64} = require('@rapidsai/cudf');
+
+let timeout   = -1;
+let datasets  = {};
+
+function clearCachedGPUData() {
+  datasets.keys().forall(key => { datasets[key] = null; });
+};
+
+/* TODO: How do I apply a list of dtypes?
+ */
+function json_aos_to_dataframe(str, columns, _) {
+  let arr = {};
+  columns.forEach((col, ix) => {
+    const no_open_list = str.split('[\n').gather([1], false);
+    const tokenized    = no_open_list.split('},');
+    const parse_result = tokenized._col.getJSONObject('.' + columns[ix]);
+    arr[col]           = Series.new(parse_result);
+  });
+  const result = new DataFrame(arr);
+  return result;
+}
+/* TODO: How do I apply a list of dtypes?
+ */
+function json_aoa_to_dataframe(str, dtypes) {
+  let arr = {};
+  dtypes.forEach((_, ix) => {
+    const no_open_list = str.split('[\n').gather([1], false);
+    const tokenized    = no_open_list.split('],');
+    const get_ix       = `[${ix}]`;
+    const parse_result = tokenized._col.getJSONObject(get_ix);
+    arr[ix]            = Series.new(parse_result);
+  });
+  const result = new DataFrame(arr);
+  return result;
+}
+
+module.exports = {
+setDataframe(name, dataframe) {
+  if (timeout) { clearTimeout(timeout); }
+  timeout        = setTimeout(clearCachedGPUData, 10 * 60 * 1000);
+  datasets[name] = dataframe;
+  console.log(datasets);
+},
+getDataframe(name) { return datasets[name] },
+readGraphology(path) {
+  console.log('readGraphology');
+  const dataset = StringSeries.read_text(path, '');
+  let split       = dataset.split('"tags":');
+  const ttags     = split.gather([1], false);
+  let rest        = split.gather([0], false);
+  split           = rest.split('"clusters":');
+  const tclusters = split.gather([1], false);
+  rest            = split.gather([0], false);
+  split           = rest.split('"edges":');
+  const tedges    = split.gather([1], false);
+  rest            = split.gather([0], false);
+  split           = rest.split('"nodes":');
+  const tnodes    = split.gather([1], false);
+  const tags = json_aos_to_dataframe(ttags, ['key', 'image'], [new Utf8String, new Utf8String]);
+  const clusters = json_aos_to_dataframe(
+    tclusters, ['key', 'color', 'clusterLabel'], [new Int32, new Utf8String, new Utf8String]);
+  const nodes =
+    json_aos_to_dataframe(tnodes, ['key', 'label', 'tag', 'URL', 'cluster', 'x', 'y', 'score'], [
+      new Utf8String,
+      new Utf8String,
+      new Utf8String,
+      new Utf8String,
+      new Int32,
+      new Float64,
+      new Float64,
+      new Int32
+    ]);
+  const edges = json_aoa_to_dataframe(tedges, [new Utf8String, new Utf8String]);
+  return {nodes: nodes, edges:edges, tags:tags, clusters:clusters};
+}
+}
