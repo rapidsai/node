83413a3eb632a42dd14e102f8f2844bfef4b9f7f
ptaylor <paul.e.taylor@me.com>
2022-08-17 10:41:18 -0700

    replace the old version w/ the new version in more files

--- scripts/postversion/linux.sh
+++ scripts/postversion/linux.sh
@@ -12,3 +12,8 @@ lerna version \
 # Replace ^ with ~
 find modules -type f -name 'package.json' -exec \
     sed -i -E -e 's+(@rapidsai/.*)": "\^+\1": "~+g' {} \;
+
+sed -i -E -e "s/$npm_old_version/$npm_package_version/g" USAGE.md
+sed -i -E -e "s/$npm_old_version/$npm_package_version/g" docker-compose.devel.yml
+sed -i -E -e "s/$npm_old_version/$npm_package_version/g" docker-compose.runtime.yml
+sed -i -E -e "s/$npm_old_version/$npm_package_version/g" .github/workflows/release.yml
