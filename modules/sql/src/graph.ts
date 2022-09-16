1984082ccf35e19f9da1e06626a4e5029a5c1854
ptaylor <paul.e.taylor@me.com>
2022-09-15 22:31:10 -0700

    fix compile error

--- modules/sql/src/graph.ts
+++ modules/sql/src/graph.ts
@@ -18,27 +18,50 @@ import {DataFrame, Table} from '@rapidsai/cudf';
 
 let nonce = Math.random() * 1e3 | 0;
 
-export class ExecutionGraph {
+export class ExecutionGraph implements Promise<DataFrame[]> {
   constructor(private _graph?: import('./rapidsai_sql').ExecutionGraph) {}
 
-  start(): void { this._graph?.start(); }
+  get[Symbol.toStringTag]() { return 'ExecutionGraph'; }
 
-  then() { return this.result(); }
-
-  async result() {
-    const {names, tables} =
-      this._graph ? (await this._graph.result()) : {names: [], tables: [new Table({})]};
-    const results: DataFrame[] = [];
-    tables.forEach((table: Table) => {
-      results.push(new DataFrame(
-        names.reduce((cols, name, i) => ({...cols, [name]: table.getColumnByIndex(i)}), {})));
-    });
-
-    return results;
+  then<TResult1 = DataFrame[], TResult2 = never>(
+    onfulfilled?: ((value: DataFrame[]) => TResult1 | PromiseLike<TResult1>)|undefined|null,
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>)|undefined|
+    null): Promise<TResult1|TResult2> {
+    return this.result().then(onfulfilled, onrejected);
   }
 
-  async sendTo(id: number) {
-    return await this.result().then((dfs) => {
+  catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>)|undefined|
+                         null): Promise<DataFrame[]|TResult> {
+    return this.result().catch(onrejected);
+  }
+
+  finally(onfinally?: (() => void)|undefined|null): Promise<DataFrame[]> {
+    return this.result().finally(onfinally);
+  }
+
+  private _result: Promise<DataFrame[]>|undefined;
+
+  start() { this._graph?.start(); }
+
+  result() {
+    if (!this._result) {
+      this._result = (async () => {
+        const {names, tables} =
+          this._graph ? (await this._graph.result()) : {names: [], tables: [new Table({})]};
+        const results: DataFrame[] = [];
+        tables.forEach((table: Table) => {
+          results.push(new DataFrame(
+            names.reduce((cols, name, i) => ({...cols, [name]: table.getColumnByIndex(i)}), {})));
+        });
+
+        return results;
+      })();
+    }
+    return this._result;
+  }
+
+  sendTo(id: number) {
+    return this.then((dfs) => {
       const {_graph}                                  = this;
       const inFlightTables: Record<string, DataFrame> = {};
       if (_graph) {
