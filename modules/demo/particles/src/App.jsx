2036008abb586ccaa55d5c07268d3b8a035b7d30
H. Thomson Comer <thomcom@gmail.com>
2022-10-06 09:36:07 -0500

    Add state reducer.

--- modules/demo/particles/src/App.jsx
+++ modules/demo/particles/src/App.jsx
@@ -6,35 +6,79 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import ParticlesView from './ParticlesView';
 import BackgroundView from './BackgroundView';
 
+const initialState = {
+  mouseX: 0,
+  mouseY: 0,
+  isHeld: false,
+  zoomLevel: 1.0
+}
+
+const toggle = previous => !previous;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'MOUSE_MOVE':
+      return {
+        ...state,
+        mouseX: action.event.screenX,
+        mouseY: action.event.screenY,
+      }
+    case 'MOUSE_CLICK':
+      console.log('click');
+      return {
+        ...state,
+        isHeld: toggle()
+      }
+    case 'SCROLL':
+      const zoom = action.event.deltaY > 0 ? 1 : -1;
+      const result = state.zoomLevel + zoom;
+      return {
+        ...state,
+        zoomLevel: result
+      }
+    default:
+      return {
+        ...state,
+        isHeld: false
+      }
+  }
+}
+
 function App({ scene }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const mouseState = () => {
+    return appState.mouse;
+  }
   const overHandler = (event) => {
-    console.log('mouse over');
-    console.log(event);
+    dispatch({ type: 'MOUSE_MOVE', event: event });
   }
   const scrollHandler = (event) => {
-    console.log('scroll');
-    console.log(event);
+    dispatch({ type: 'SCROLL', event: event });
   }
   const clickHandler = (event) => {
-    console.log('click');
-    console.log(event);
+    dispatch({ type: 'MOUSE_CLICK', event: event });
+  }
+  const releaseHandler = (event) => {
+    dispatch({ type: 'MOUSE_RELEASE', event: event });
   }
   useEffect(() => {
     // subscribe event
     window.addEventListener("wheel", scrollHandler);
     window.addEventListener("mousemove", overHandler);
     window.addEventListener("mousedown", clickHandler);
+    window.addEventListener("mouseup", releaseHandler);
     return () => {
       // unsubscribe event
       window.removeEventListener("wheel", scrollHandler);
       window.removeEventListener("mousemove", overHandler);
       window.removeEventListener("mousedown", clickHandler);
+      window.removeEventListener("mouseup", releaseHandler);
     };
   }, []);
 
