9b61542bec778dd7cc43b9955dd5f09a09f2e678
H. Thomson Comer <thomcom@gmail.com>
2022-10-12 13:47:56 -0500

    Writing fetch for server routine now.

--- modules/demo/particles/src/index.js
+++ modules/demo/particles/src/index.js
@@ -37,7 +37,24 @@ window.addEventListener('mousemove', (event) => {
 
 setInterval(() => {props.angle = (props.angle + 1)}, 16);
 
-points(props);
-background(props);
+const SERVER           = 'http://localhost';
+const PORT             = '3010';
+const READ_CSV_URL     = '/gpu/DataFrame/ReadCSV';
+const READ_CSV_OPTIONS = {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: 'points.csv'
+};
+const FETCH_POINTS_URL = '/particles/get_shader_column';
+
+(async () => {
+  try {
+    const readCsvResult = await fetch(SERVER + ':' + PORT + READ_CSV_URL);
+    const remotePoints  = await fetch(SERVER + ':' + PORT + FETCH_POINTS_URL);
+  } finally {
+    points(props);
+    background(props);
+  }
+})();
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
