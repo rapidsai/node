7401333851ad03ab23f941b54682ba14fe672ba8
H. Thomson Comer <thomcom@gmail.com>
2022-11-21 14:32:45 -0600

    Add new test files.

---
+++ modules/demo/api-server/test/routes/particles.test.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2022, NVIDIA CORPORATION.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict'
+
+const {dir}                                   = require('console');
+const {test}                                  = require('tap');
+const {build}                                 = require('../helper');
+const {tableFromIPC, RecordBatchStreamWriter} = require('apache-arrow');
+const {json_large, json_good, json_out_of_order, json_bad_map, csv_base} =
+  require('../fixtures.js');
+const gpu_cache = require('../../util/gpu_cache');
+
+test('read_csv', async (t) => {
+  const dir   = t.testdir(csv_base);
+  const rpath = 'test/routes/' + dir.substring(dir.lastIndexOf('/'));
+  const app   = await build(t);
+  gpu_cache._setPathForTesting(rpath);
+  const res = await app.inject(
+    {method: 'POST', url: '/gpu/DataFrame/readCSV', body: {filename: 'csv_base.csv'}});
+  const release = await app.inject({method: 'POST', url: '/graphology/release'});
+  t.equal(res.statusCode, 200);
+  t.same(JSON.parse(res.payload), {
+    success: true,
+    message: 'CSV file in GPU memory.',
+    statusCode: 200,
+    params: {filename: 'csv_base.csv'}
+  });
+});
