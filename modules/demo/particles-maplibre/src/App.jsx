b8e1106579af92d2d3ecdb7c5c23b0fe9788cb43
Thomson Comer <thomcom@gmail.com>
2023-02-27 13:49:09 -0600

    Cleaning up state.

--- modules/demo/particles-maplibre/src/App.jsx
+++ modules/demo/particles-maplibre/src/App.jsx
@@ -23,81 +23,14 @@ import Particles from './Particles.jsx';
 import reducer from './Reducer';
 import Title from './Title.js';
 
-/*
-  Homogeneous coordinates for projection matrix
-  Projecting from one 2d basis to another using control points
-  requires 3 3d coordinates in order to capture translation in
-  the projection matrix. OpenGL is a 3d system with a 4d
-  homogeneous matrix patter. We'll use mat4 to do 4d matrix
-  transformations on our 2d points. Throw away the third dimension
-  and keep the homogeneous coordinate.
-
-  These will be used in props to align the screen and world coordinate
-  systems.
-*/
-const worldCoords = [
-  -137.3,
-  49.2,  // top right
-  0,
-  1,
-  -61.6,
-  49.2,  // top left
-  0,
-  1,
-  -61.6,
-  25.2,  // bottom left
-  0,
-  1,
-  -137.3,
-  25.2,  // bottom right
-  0,
-  1
-];
-/*
- Screen coords matching the axes of the above worldCoords. They are used to
- compute the world view to screen projection matrix.
-
- These are inadequate for aligning the mouse movements correctly with the world movement, but they
- provide enough information to create a viewPort alignment. TODO
- */
-// TODO: Change dynamically to the clientWidth and clientHeight
-// that are available when the component is mounted.
-const screenCoords = [
-  document.documentElement.clientWidth,
-  document.documentElement.clientHeight,
-  0,
-  1,
-  0,
-  document.documentElement.clientHeight,
-  0,
-  1,
-  0,
-  0,
-  0,
-  1,
-  document.documentElement.clientWidth,
-  0,
-  0,
-  1
-];
-
 const initialState = {
   /*
    All application state except for a few constants
    are defined here. This state must be shared between
    components.
    */
-  // Define world coords
-  w: () => [...worldCoords],
-  // Define screen coords
-  s: () => [...screenCoords],
   zoomLevel: 2.0,
   angle: 0,
-  screenWidth: document.documentElement.clientHeight,
-  screenHeight: document.documentElement.clientWidth,
-  centerX: document.documentElement.clientWidth / 2.0,
-  centerY: document.documentElement.clientHeight / 2.0,
-  currentWorldCoords: { xmin: undefined, xmax: undefined, ymin: undefined, ymax: undefined },
   fetching: false,
   pointBudget: 180000000,
   pointsPerRequest: 1000000,
@@ -134,9 +67,6 @@ function App() {
 
   // subscribe event
   useEffect(() => {
-    const overHandler = (event) => {
-      dispatch({ type: 'MOUSE_MOVE', event: event });
-    }
     const scrollHandler = (event) => {
       dispatch({ type: 'SCROLL', event: event });
     }
@@ -148,14 +78,12 @@ function App() {
       dispatch({ type: 'MOUSE_RELEASE', event: event });
     }
     window.addEventListener("wheel", scrollHandler);
-    window.addEventListener("mousemove", overHandler);
     window.addEventListener("mousedown", clickHandler);
     window.addEventListener("mouseup", releaseHandler);
 
     return () => {
       // unsubscribe event
       window.removeEventListener("wheel", scrollHandler);
-      window.removeEventListener("mousemove", overHandler);
       window.removeEventListener("mousedown", clickHandler);
       window.removeEventListener("mouseup", releaseHandler);
     };
--- modules/demo/particles-maplibre/src/matrices.js
+++ modules/demo/particles-maplibre/src/matrices.js
@@ -21,63 +21,6 @@ export const getProjection = (space1, space2) => {
 };
 
 /*
- Given world and screen bounds and the current zoom level,
- compute new world bounds from the screen bounds.
- */
-export const getScreenToWorldCoords = (props) => {
-  const A            = getProjection(props.w(), props.s());
-  const orthoScale   = getCurrentOrthoScale(props);
-  const screenBounds = [
-    props.centerX + props.screenWidth / 2.0 * orthoScale,
-    props.centerY + props.screenWidth / 2.0 * orthoScale,
-    0,
-    1,  // top right
-    props.centerX - props.screenWidth / 2.0 * orthoScale,
-    props.centerY + props.screenWidth / 2.0 * orthoScale,
-    0,
-    1,  // top left
-    props.centerX - props.screenWidth / 2.0 * orthoScale,
-    props.centerY - props.screenWidth / 2.0 * orthoScale,
-    0,
-    1,  // bottom left
-    props.centerX + props.screenWidth / 2.0 * orthoScale,
-    props.centerY - props.screenWidth / 2.0 * orthoScale,
-    0,
-    1,  // bottom right
-  ];
-  const worldBounds = mat4.multiply([], A, screenBounds);
-  return worldBounds;
-};
-
-/*
- The points view matrix. Centers the points based on the current screen
- center, which needs viewport adjustments. TODO.
- */
-export const getPointsViewMatrix = (props) => {
-  const screenLookAt = [props.centerX, props.centerY, 0, 1];
-  const A            = getProjection(props.w(), props.s());
-  const lookAtWorld  = mat4.multiply([], A, screenLookAt);
-  const lookAtFinal  = mat4.lookAt(
-    [], [lookAtWorld[0], lookAtWorld[1], 10], [lookAtWorld[0], lookAtWorld[1], 0], [0, -1, 0]);
-  return lookAtFinal;
-};
-
-/*
- The background view matrix. The same as the points view matrix, with some rescaling.
- Needs a better viewport correspondence for click and drag events.
- */
-export const getBackgroundViewMatrix = (props) => {
-  const screenLookAt = [-props.centerX, props.centerY, 1, 1];
-  const A            = getProjection(props.w(), props.s());
-  const lookAtWorld  = mat4.multiply([], A, screenLookAt);
-  const lookAtFinal  = mat4.lookAt(
-    [], [lookAtWorld[0], lookAtWorld[1], 10], [lookAtWorld[0], lookAtWorld[1], 0], [0, -1, 0]);
-  const scaled     = mat4.scale([], lookAtFinal, [1.0, 1.3, 1]);
-  const translated = mat4.translate([], scaled, [64, -5, 0]);
-  return translated;
-};
-
-/*
  A function that computes a zoom level based on a zoomLevel that ranges from 0 to n.
  TODO: Currently the range of the zoomLevel can be negative. This should be corrected.
  */
